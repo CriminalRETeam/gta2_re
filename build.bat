c8183c51447f9c00c7c9aa99b42d99172518c817
Paul <github@paulsapps.com>
2024-10-18 00:27:10 +0100

    hard code le hax

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ call set_vc6
 mkdir build_vc6
 cd build_vc6
 echo %PATH%
+dir Z:\home\appveyor\projects\gta2-re\3rdParty\gta2_re_compile_tools\cmake-3.5.0-win32-x86\bin
 cmake.exe .. -G"Visual Studio 6"
-..\3rdParty\cmake-3.5.0-win32-x86\bin\cmake.exe .. -G"Visual Studio 6"
+Z:\home\appveyor\projects\gta2-re\3rdParty\gta2_re_compile_tools\cmake-3.5.0-win32-x86\bin\cmake.exe .. -G"Visual Studio 6"
 msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
