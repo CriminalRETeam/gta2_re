d7f0b5cb00ab549d052cd9934533a29f0528ad3d
Paul <github@paulsapps.com>
2024-10-18 02:26:55 +0100

    its jenova

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ mkdir build_vc6
 cd build_vc6
 echo %PATH%
 cmake.exe .. -G"Visual Studio 6"
-msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
+rem msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
