a45f775c009bd036bcd4d043e6a0adc951e0a641
Paul <github@paulsapps.com>
2024-10-25 05:08:00 +0100

    new url

--- Scripts/bin_comp/download_exe.ps1
+++ Scripts/bin_comp/download_exe.ps1
@@ -1,3 +1,2 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest 'https://www.libsdl.org/release/SDL2-devel-2.0.22-VC.zip' -OutFile "SDL2-devel-2.0.22-VC.zip"
-Invoke-WebRequest 'https://mouzedrift.s-ul.eu/KTb8N0bc' -OutFile 10.5.exe
+Invoke-WebRequest 'https://cdn.discordapp.com/attachments/1187316940532891689/1299193521177104434/10.5.exe?ex=671c4fc1&is=671afe41&hm=d017ee3e91a26587c878b65a4ccfc14083d67cd3396205ad8c58cddeb8f62d8a&' -OutFile 10.5.exe
