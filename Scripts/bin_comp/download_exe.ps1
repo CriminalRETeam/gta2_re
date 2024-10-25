aa0403aa01fd5dd6043670ca19a8f0d6ea829d43
Paul <github@paulsapps.com>
2024-10-25 04:39:40 +0100

    heh

--- Scripts/bin_comp/download_exe.ps1
+++ Scripts/bin_comp/download_exe.ps1
@@ -1,2 +1,2 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls13, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest 'https://mouzedrift.s-ul.eu/KTb8N0bc' -OutFile 10.5.exe
