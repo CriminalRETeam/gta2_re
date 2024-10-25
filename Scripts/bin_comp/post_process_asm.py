d07efc92b7adeb536adc2aeead47aef1f4d56959
Paul <github@paulsapps.com>
2024-10-26 00:14:54 +0100

    add more cases of asm that needs post processing

--- Scripts/bin_comp/post_process_asm.py
+++ Scripts/bin_comp/post_process_asm.py
@@ -22,7 +22,7 @@ def is_hex_str_in_exe_range(line):
 def get_operands(s):
     return s.split(" ")[1].split(",")
 
-def get_constant_from_single_operand(s, in_exe_range):
+def is_hex_constant(s, in_exe_range):
     tmp = s
     if tmp[0] == "$" or tmp[0] == "*":
         tmp = tmp[1:]
@@ -36,7 +36,7 @@ def get_constant_from_single_operand(s, in_exe_range):
 
 def get_constant_from_inst_generic(s, in_exe_range):
     for op in get_operands(s):
-        ret = get_constant_from_single_operand(op, in_exe_range)
+        ret = is_hex_constant(op, in_exe_range)
         if ret is not None:
             return ret   
     return None
@@ -46,7 +46,7 @@ def get_constant_from_deref(s, in_exe_range):
     if pos != -1:
         tmp = s[:pos]
         if len(tmp) > 0:
-            return get_constant_from_single_operand(tmp, in_exe_range)
+            return is_hex_constant(tmp, in_exe_range)
     return None
 
 def get_constant_from_deref_inst_generic(s, in_exe_range):
@@ -58,18 +58,26 @@ def get_constant_from_deref_inst_generic(s, in_exe_range):
 
 def extract_constant(s):
     ret = None
-    if s.startswith("movw") or s.startswith("mov"):
-        ret = get_constant_from_inst_generic(s, True)
+    if s.startswith("movw") or s.startswith("movl") or s.startswith("mov") or s.startswith("cmp"):
+        ops = s.split(" ")[1].split(",")
+        for op in ops:
+            ret = get_constant_from_deref(op, True)
+            if ret is not None:
+                break
+        if ret is None:
+            ret = get_constant_from_inst_generic(s, True)
     elif s.startswith("call") or s.startswith("calll"):
         ret = get_constant_from_inst_generic(s, False)
     elif s.startswith("jmpl"):
         ret = get_constant_from_deref(s.split(" ")[1], True)
-    elif s.startswith("push") or s.startswith("jmp"):
-        ret = get_constant_from_single_operand(s.split(" ")[1], False)
+        if ret is None:
+             ret = get_constant_from_inst_generic(s, True)
+    elif s.startswith("push") or s.startswith("jmp") or s.startswith("fmull") or s.startswith("fmuls"):
+        ret = is_hex_constant(s.split(" ")[1], False)
     elif s.startswith("lea"):
         ret = get_constant_from_deref_inst_generic(s, True)
 
-    return ret
+    return [ret]
 
 
 def replace_constants(line, constants):
@@ -108,37 +116,58 @@ def post_process_asm(asmstr):
 class TestStringMethods(unittest.TestCase):
 
     def test_movw_hex_str2(self):
-        self.assertEqual(extract_constant("movw $1,0x434A18"), "0x434A18")
+        self.assertEqual(extract_constant("movw $1,0x434A18"), ["0x434A18"])
 
     def test_mov_hex_str1(self):
-        self.assertEqual(extract_constant("mov 0x442F18,%edx"), "0x442F18")
+        self.assertEqual(extract_constant("mov 0x442F18,%edx"), ["0x442F18"])
 
     def test_mov_hex_str2(self):
-        self.assertEqual(extract_constant("mov %cx,0x434A10"), "0x434A10")
+        self.assertEqual(extract_constant("mov %cx,0x434A10"), ["0x434A10"])
 
     def test_mov_hex_str3(self):
-        self.assertEqual(extract_constant("mov $0x42AF10,%edi"), "$0x42AF10")
+        self.assertEqual(extract_constant("mov $0x42AF10,%edi"), ["$0x42AF10"])
+
+    def test_mov_hex_str4(self):
+        self.assertEqual(extract_constant("mov %dl,0x436494(%eax)"), ["0x436494"])
+
+    def test_mov_hex_str5(self):
+        self.assertEqual(extract_constant("mov 0x416000(%eax),%cl"), ["0x416000"])
+
+    #def test_mov_hex_str5(self):
+    #    self.assertEqual(extract_constant("movl $0x67D818,0x67D704"), ["$0x67D818", "0x67D704"])
 
     def test_call_hex(self):
-        self.assertEqual(extract_constant("call 0x00013BED"), "0x00013BED")
+        self.assertEqual(extract_constant("call 0x00013BED"), ["0x00013BED"])
 
     def test_calll_hex(self):
-        self.assertEqual(extract_constant("calll *0x426208"), "*0x426208")
+        self.assertEqual(extract_constant("calll *0x426208"), ["*0x426208"])
 
     def test_push_hex(self):
-        self.assertEqual(extract_constant("push $0x42A3A4"), "$0x42A3A4")
+        self.assertEqual(extract_constant("push $0x42A3A4"), ["$0x42A3A4"])
 
-    def test_jmpl_hex(self):
-        self.assertEqual(extract_constant("jmpl *0x40FC68(,%eax,4)"), "*0x40FC68")
+    def test_jmpl_hex1(self):
+        self.assertEqual(extract_constant("jmpl *0x40FC68(,%eax,4)"), ["*0x40FC68"])
+
+    def test_jmpl_hex2(self):
+        self.assertEqual(extract_constant("jmpl *0x43656C"), ["*0x43656C"])
 
     def test_jmp_hex(self):
-        self.assertEqual(extract_constant("jmp 0xFFFFFC60"), "0xFFFFFC60")
+        self.assertEqual(extract_constant("jmp 0xFFFFFC60"), ["0xFFFFFC60"])
 
     def test_lea_hex1(self):
-        self.assertEqual(extract_constant("lea 0x4262B8(,%ecx,2),%edi"), "0x4262B8")
+        self.assertEqual(extract_constant("lea 0x4262B8(,%ecx,2),%edi"), ["0x4262B8"])
 
     def test_lea_hex2(self):
-        self.assertEqual(extract_constant("lea (%ecx,%eax,8),%eax"), None)
+        self.assertEqual(extract_constant("lea (%ecx,%eax,8),%eax"), [])
+
+    def test_lea_hex2(self):
+        self.assertEqual(extract_constant("cmp %bl,0x43D0F7"), ["0x43D0F7"])
+
+    def test_fmull_hex(self):
+        self.assertEqual(extract_constant("fmull 0x427F00"), ["0x427F00"])
+
+    def test_fmuls_hex(self):
+        self.assertEqual(extract_constant("fmuls 0x5FE3C8"), ["0x5FE3C8"])
 
 
-#unittest.main()
+unittest.main()
