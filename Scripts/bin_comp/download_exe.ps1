d15bc35b09b34ee7456ec525cc36d8f334f02207
Paul <github@paulsapps.com>
2024-10-25 04:46:12 +0100

    does this work??

--- Scripts/bin_comp/download_exe.ps1
+++ Scripts/bin_comp/download_exe.ps1
@@ -1,2 +1,3 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest 'https://www.libsdl.org/release/SDL2-devel-2.0.22-VC.zip' -OutFile "SDL2-devel-2.0.22-VC.zip"
 Invoke-WebRequest 'https://mouzedrift.s-ul.eu/KTb8N0bc' -OutFile 10.5.exe
