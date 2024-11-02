b36a64ec0acd6c4439387f701f74f8ff77e95f95
Paul <github@paulsapps.com>
2024-11-02 21:01:45 +0000

    stub Game_0x40

--- Source/Game_0x40.cpp
+++ Source/Game_0x40.cpp
@@ -2,8 +2,182 @@
 
 Game_0x40* gGame_0x40_67E008;
 
-// stub
+s32 Game_0x40::sub_4B8BB0()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B8BD0(s32 arg0, s32 main_state, s8 a2)
+{
+
+}
+
+void Game_0x40::sub_4B8C00(s32 a1, s32 a2)
+{
+
+}
+
+s8 Game_0x40::sub_4B8C20()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B8C40()
+{
+
+}
+
+s32 Game_0x40::sub_4B8E00(u32 a1, u32 a2)
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B8E50()
+{
+
+}
+
+void Game_0x40::sub_4B8EB0()
+{
+
+}
+
+void Game_0x40::ShowCounters_4B8FF0()
+{
+
+}
+
+s8 Game_0x40::sub_4B9270(u16 *a2) // TODO: user call 2nd param
+{
+    return 0;
+}
+
+void Game_0x40::Draw_4B92D0()
+{
+
+}
+
+s8 Game_0x40::sub_4B9380()
+{
+    return 0;
+}
+
+s32 Game_0x40::sub_4B93C0()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B9410()
+{
+
+}
+
+s8 Game_0x40::sub_4B9640()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B9700()
+{
+
+}
+
+void Game_0x40::sub_4B9710()
+{
+
+}
+
 void Game_0x40::sub_4B9720()
+{
+
+}
+
+angry_lewin_0x85C *Game_0x40::sub_4B9750()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B9790(s32 a2, s32 a3, s32 a4)
+{
+
+}
+
+s8 Game_0x40::sub_4B97E0(Car_3C *a2, s32 a3)
+{
+    return 0;
+}
+
+s8 Game_0x40::sub_4B9830(Car_3C *pCarSprite, s32 a3)
+{
+    return 0;
+}
+
+s8 Game_0x40::sub_4B9890(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+bool Game_0x40::sub_4B98E0(s16 *a2, u8 playerIdx, s32 a4)
+{
+    return false;
+}
+
+s8 Game_0x40::sub_4B9950(Car_3C *pCarSprite, u8 idx, s32 a4)
+{
+    return 0;
+}
+
+bool Game_0x40::sub_4B9A10(Car_3C *a2, u8 playerIdx)
+{
+    return false;
+}
+
+s8 Game_0x40::is_point_on_screen_4B9A80(s32 a2_fp, s32 a3_fp)
+{
+    return 0;
+}
+
+s8 Game_0x40::sub_4B9B10(s32 *pBounds)
+{
+    return 0;
+}
+
+DrawUnk_0xBC *Game_0x40::IteratePlayerCamera_4B9BC0()
+{
+    return 0;
+}
+
+s8 Game_0x40::sub_4B9C10(Car_BC *a2)
+{
+    return 0;
+}
+
+DrawUnk_0xBC* Game_0x40::sub_4B9C50()
+{
+    return 0;
+}
+
+angry_lewin_0x85C* Game_0x40::sub_4B9CD0()
+{
+    return 0;
+}
+
+angry_lewin_0x85C* Game_0x40::IterateNextPlayer_4B9D10()
+{
+    return 0;
+}
+
+void Game_0x40::sub_4B9D60(angry_lewin_0x85C *a2)
+{
+
+}
+
+Game_0x40::Game_0x40(u8 max_players, s8 player_idx) // 4B9DE0
+{
+
+}
+
+Game_0x40::~Game_0x40() // 4BAE30 
 {
 
 }
