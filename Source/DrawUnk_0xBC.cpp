9ae30189060c75323ed61abeecdc99a159ef8077
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-03 19:46:18 +0100

    DrawUnk_0xBC stubs

---
+++ Source/DrawUnk_0xBC.cpp
@@ -0,0 +1,183 @@
+#include "DrawUnk_0xBC.hpp"
+#include "Function.hpp"
+
+STUB_FUNC(0x4355D0)
+char_type DrawUnk_0xBC::sub_4355D0(Car_3C* a2)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435630)
+char_type DrawUnk_0xBC::sub_435630(s16* a2, s32 a3)
+{
+	return 0;
+}
+
+STUB_FUNC(0x4357B0)
+s32 DrawUnk_0xBC::sub_4357B0()
+{
+	return 0;
+}
+
+STUB_FUNC(0x4357F0)
+s32 DrawUnk_0xBC::sub_4357F0()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435810)
+s32 DrawUnk_0xBC::sub_435810()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435830)
+s32 DrawUnk_0xBC::sub_435830()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435840)
+s32 DrawUnk_0xBC::sub_435840()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435860)
+s32 DrawUnk_0xBC::sub_435860(DrawUnk_0xBC* a2)
+{
+	return 0;
+}
+
+STUB_FUNC(0x4358D0)
+s16* DrawUnk_0xBC::sub_4358D0(s16* a2)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435A20)
+s32* DrawUnk_0xBC::sub_435A20(s32* a2)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435A70)
+s32 DrawUnk_0xBC::sub_435A70(s32 a2, s32 a3, s32 a4)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435B90)
+s32 DrawUnk_0xBC::sub_435B90()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435D20)
+s32 DrawUnk_0xBC::sub_435D20(char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435DD0)
+s32 DrawUnk_0xBC::sub_435DD0()
+{
+	return 0;
+}
+
+STUB_FUNC(0x435F90)
+u8 DrawUnk_0xBC::sub_435F90(s32 a2)
+{
+	return 0;
+}
+
+STUB_FUNC(0x435FF0)
+s32 DrawUnk_0xBC::sub_435FF0()
+{
+	return 0;
+}
+
+STUB_FUNC(0x436110)
+s32 DrawUnk_0xBC::sub_436110()
+{
+	return 0;
+}
+
+STUB_FUNC(0x436120)
+void DrawUnk_0xBC::sub_436120(s32 a2)
+{
+
+}
+
+STUB_FUNC(0x436140)
+long long DrawUnk_0xBC::sub_436140()
+{
+	return 0;
+}
+
+STUB_FUNC(0x4361B0)
+u32 DrawUnk_0xBC::sub_4361B0(u32 a2, u32 a3)
+{
+	return 0;
+}
+
+STUB_FUNC(0x436200)
+s32 DrawUnk_0xBC::sub_436200(s32 a2, s32* a3, s32* a4, s32* a5)
+{
+	return 0;
+}
+
+STUB_FUNC(0x4364A0)
+void DrawUnk_0xBC::sub_4364A0(s32 a2)
+{
+
+}
+
+STUB_FUNC(0x436540)
+void DrawUnk_0xBC::sub_436540(s32* a2)
+{
+
+}
+
+STUB_FUNC(0x436710)
+char_type DrawUnk_0xBC::sub_436710(char_type a2, char_type a3, char_type a4, char_type a5)
+{
+	return 0;
+}
+
+STUB_FUNC(0x436830)
+s32 DrawUnk_0xBC::sub_436830()
+{
+	return 0;
+}
+
+STUB_FUNC(0x436860)
+long long DrawUnk_0xBC::sub_436860(s32 a2, s32* a3, s32* a4, s32 a5)
+{
+	return 0;
+}
+
+STUB_FUNC(0x4368E0)
+DrawUnk_0xBC::DrawUnk_0xBC() // 4368E0
+{
+
+}
+
+STUB_FUNC(0x4369E0)
+DrawUnk_0xBC::~DrawUnk_0xBC() // empty 4369E0
+{
+
+}
+
+STUB_FUNC(0x4397D0)
+s32 DrawUnk_0xBC::sub_4397D0(s32 a2, s32 a3, s32 a4, s32 a5)
+{
+	return 0;
+}
+
+STUB_FUNC(0x58CF10)
+s32 DrawUnk_0xBC::sub_58CF10(s32 a2, s32 a3)
+{
+	return 0;
+}
+
---
+++ Source/DrawUnk_0xBC.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "types.hpp"
+
+// TODO: add later
+class Car_3C;
+
+class DrawUnk_0xBC
+{
+	EXPORT char_type sub_4355D0(Car_3C* a2);
+	EXPORT char_type sub_435630(s16* a2, s32 a3);
+	EXPORT s32 sub_4357B0();
+	EXPORT s32 sub_4357F0();
+	EXPORT s32 sub_435810();
+	EXPORT s32 sub_435830();
+	EXPORT s32 sub_435840();
+	EXPORT s32 sub_435860(DrawUnk_0xBC* a2);
+	EXPORT s16* sub_4358D0(s16* a2);
+	EXPORT s32* sub_435A20(s32* a2);
+	EXPORT s32 sub_435A70(s32 a2, s32 a3, s32 a4);
+	EXPORT s32 sub_435B90();
+	EXPORT s32 sub_435D20( char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7);
+	EXPORT s32 sub_435DD0();
+	EXPORT u8 sub_435F90(s32 a2);
+	EXPORT s32 sub_435FF0();
+	EXPORT s32 sub_436110();
+	EXPORT void sub_436120(s32 a2);
+	EXPORT long long sub_436140();
+	EXPORT u32 sub_4361B0(u32 a2, u32 a3);
+	EXPORT s32 sub_436200(s32 a2, s32* a3, s32* a4, s32* a5);
+	EXPORT void sub_4364A0(s32 a2);
+	EXPORT void sub_436540(s32* a2);
+	EXPORT char_type sub_436710(char_type a2, char_type a3, char_type a4, char_type a5);
+	EXPORT s32 sub_436830();
+	EXPORT long long sub_436860(s32 a2, s32* a3, s32* a4, s32 a5);
+	EXPORT DrawUnk_0xBC(); // 4368E0
+	EXPORT ~DrawUnk_0xBC(); // empty 4369E0
+	EXPORT s32 sub_4397D0(s32 a2, s32 a3, s32 a4, s32 a5);
+	EXPORT s32 sub_58CF10(s32 a2, s32 a3);
+
+
+	s32 field_0;
+	s32 field_4_unk;
+	s32 field_8;
+	s16 field_C;
+	s16 field_E;
+	s32 field_10;
+	s32 field_14;
+	s32 field_18;
+	s32 field_1C;
+	s32 field_20_right;
+	s32 field_24_left;
+	s32 field_28_bottom;
+	s32 field_2C_top;
+	s32 field_30;
+	s32 field_34;
+	s32 field_38;
+	s32 field_3C;
+	s32 field_40;
+	char_type field_44;
+	char_type field_45;
+	char_type field_46;
+	char_type field_47;
+	s32 field_48;
+	s32 field_4C;
+	s32 field_50;
+	s32 field_54;
+	s32 field_58;
+	s32 field_5C;
+	s32 field_60;
+	s32 field_64;
+	s32 field_68;
+	s32 field_6C;
+	s32 field_70;
+	s32 field_74;
+	s32 field_78;
+	s32 field_7C;
+	s32 field_80;
+	s32 field_84;
+	s32 field_88;
+	s32 field_8C;
+	s32 field_90;
+	s32 field_94;
+	s32 field_98_x;
+	s32 field_9C_y;
+	s32 field_A0_z;
+	s32 field_A4;
+	s32 field_A8;
+	s32 field_AC;
+	s32 field_B0;
+	s32 field_B4;
+	s32 field_B8;
+};
+
