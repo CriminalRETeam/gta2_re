450ab46551f3f8c4a55464128e4f559ebac62c62
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-05 00:12:06 +0100

    remove redundant class scope

--- Source/Car_10.hpp
+++ Source/Car_10.hpp
@@ -7,12 +7,12 @@
 class Car_10
 {
 public:
-    Car_10* Car_10::sub_439CD0(s32* a2);
-    void Car_10::sub_439D40(Car_10* a1, s32 a2, s32* a3);
-    s32 Car_10::sub_439DA0(s32 edx0, s32* a3);
-    s32 Car_10::sub_439E40(u8 a2);
-    s32 Car_10::sub_439E60();
-    s32 Car_10::sub_439EA0();
+    Car_10* sub_439CD0(s32* a2);
+    void sub_439D40(Car_10* a1, s32 a2, s32* a3);
+    s32 sub_439DA0(s32 edx0, s32* a3);
+    s32 sub_439E40(u8 a2);
+    s32 sub_439E60();
+    s32 sub_439EA0();
     Car_10(); // 447330
     ~Car_10(); // 447350
 
