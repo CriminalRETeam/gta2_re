f963928c7cee97151b6ecd8c3f31938b8ecd5f08
Paul <github@paulsapps.com>
2024-11-04 02:50:01 +0000

    fix build settings

--- build.bat
+++ build.bat
@@ -4,8 +4,9 @@ call set_vc6
 mkdir build_vc6
 cd build_vc6
 rem echo %PATH%
-cmake.exe .. -G"NMake Makefiles JOM"
+cmake.exe -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF -DCMAKE_BUILD_TYPE=Release .. -G"NMake Makefiles JOM"
+rem cmake.exe -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .. -G"Visual Studio 6"
 rem msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
 rem msdev decomp.dsw /MAKE "gta2_dll_exports - Win32 Release"
 rem msdev decomp.dsw /MAKE "gta2_dll_imports - Win32 Release"
-cmake.exe --build . --config Release
+cmake --build . --target all
