9eacc91444273ee1103ddc690d50bf98e4ad27cd
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-04 17:15:27 +0100

    stub Car_BC, Car_10, Ped_Unknown_4

---
+++ Source/Car_10.cpp
@@ -0,0 +1,50 @@
+#include "Car_10.hpp"
+
+STUB_FUNC(0x439CD0)
+Car_10* Car_10::sub_439CD0(s32* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x439D40)
+void Car_10::sub_439D40(Car_10* a1, s32 a2, s32* a3)
+{
+
+}
+
+STUB_FUNC(0x439DA0)
+s32 Car_10::sub_439DA0(s32 edx0, s32* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x439E40)
+s32 Car_10::sub_439E40(u8 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x439E60)
+s32 Car_10::sub_439E60()
+{
+    return 0;
+}
+
+STUB_FUNC(0x439EA0)
+s32 Car_10::sub_439EA0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x447330)
+Car_10::Car_10()
+{
+
+}
+
+STUB_FUNC(0x447350)
+Car_10::~Car_10()
+{
+
+}
+
---
+++ Source/Ped_Unknown_4.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "types.hpp"
+#include "Function.hpp"
+
+class cool_nash_0x294;
+class Char_8;
+class Car_BC;
+
+class Ped_Unknown_4
+{
+public:
+    EXPORT Char_8* sub_471140(cool_nash_0x294* pPed);
+    EXPORT Char_8* sub_471160(cool_nash_0x294* a2);
+    EXPORT Char_8* sub_4711F0(cool_nash_0x294* a2);
+    EXPORT char_type sub_471290();
+    EXPORT cool_nash_0x294* sub_471320();
+    EXPORT Char_8** sub_4715A0();
+    EXPORT Char_8** sub_4715E0();
+    EXPORT cool_nash_0x294* sub_471630();
+    EXPORT Char_8* sub_471680();
+    EXPORT s32 sub_4716B0();
+    EXPORT Char_8* sub_4716D0(Car_BC* a2);
+    EXPORT char_type sub_471710();
+
+    Char_8* field_0_pOwner;
+};
+GTA2_ASSERT_SIZEOF_ALWAYS(Ped_Unknown_4, 0x4)
