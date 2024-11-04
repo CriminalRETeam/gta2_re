2e07e5affa4c78493751af2ee8f98e7d8f2806ea
Paul <github@paulsapps.com>
2024-11-04 03:32:09 +0000

    again try to appease wine

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ call set_vc6
 mkdir build_vc6
 cd build_vc6
 rem echo %PATH%
-cmake.exe -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF -DCMAKE_BUILD_TYPE=Release .. -G"NMake Makefiles JOM"
+cmake.exe -DCMAKE_MAKE_PROGRAM=jom.exe -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF -DCMAKE_BUILD_TYPE=Release .. -G"NMake Makefiles JOM"
 rem cmake.exe -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .. -G"Visual Studio 6"
 rem msdev decomp.dsw /MAKE "decomp_main - Win32 Release"
 rem msdev decomp.dsw /MAKE "gta2_dll_exports - Win32 Release"
