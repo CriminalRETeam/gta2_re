467dcadbd5e6e1eac86984b8cf76302aad1c2522
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-04 11:27:50 +0100

    some DrawUnk_0xBC matches

--- Source/DrawUnk_0xBC.cpp
+++ Source/DrawUnk_0xBC.cpp
@@ -13,40 +13,68 @@ char_type DrawUnk_0xBC::sub_435630(s16* a2, s32 a3)
 	return 0;
 }
 
-STUB_FUNC(0x4357B0)
-s32 DrawUnk_0xBC::sub_4357B0()
+MATCH_FUNC(0x4357B0)
+void DrawUnk_0xBC::sub_4357B0()
 {
-	return 0;
+	field_88 = field_98_x;
+	field_8C = field_9C_y;
+	field_90 = field_A0_z;
+	field_94 = field_A4;
 }
 
-STUB_FUNC(0x4357F0)
-s32 DrawUnk_0xBC::sub_4357F0()
+s32 dword_676894;
+s32 dword_676678;
+
+MATCH_FUNC(0x4357F0)
+void DrawUnk_0xBC::sub_4357F0()
 {
-	return 0;
+	if (field_40 < dword_676894)
+	{
+		field_40 += dword_676678;
+	}
 }
+
+s32 dword_676818;
 
 STUB_FUNC(0x435810)
-s32 DrawUnk_0xBC::sub_435810()
+void DrawUnk_0xBC::sub_435810()
 {
-	return 0;
+	if (field_40 > dword_676818)
+	{
+		field_40 -= dword_676678;
+	}
 }
 
-STUB_FUNC(0x435830)
-s32 DrawUnk_0xBC::sub_435830()
+MATCH_FUNC(0x435830)
+void DrawUnk_0xBC::sub_435830()
 {
-	return 0;
+	field_40 = dword_676818;
 }
 
-STUB_FUNC(0x435840)
-s32 DrawUnk_0xBC::sub_435840()
+s32 dword_676898;
+
+MATCH_FUNC(0x435840)
+void DrawUnk_0xBC::sub_435840()
 {
-	return 0;
+	if (field_18 < dword_676818)
+	{
+		field_18 = dword_676818;
+	}
+
+	if (field_18 > dword_676898)
+	{
+		field_18 = dword_676898;
+	}
 }
 
-STUB_FUNC(0x435860)
-s32 DrawUnk_0xBC::sub_435860(DrawUnk_0xBC* a2)
+MATCH_FUNC(0x435860)
+void DrawUnk_0xBC::sub_435860(DrawUnk_0xBC* a2)
 {
-	return 0;
+	field_10 += a2->field_98_x - a2->field_88;
+	field_14 += a2->field_9C_y - a2->field_8C;
+	field_18 += a2->field_A0_z - a2->field_90;
+	field_1C += a2->field_A4 - a2->field_94;
+	sub_435840();
 }
 
 STUB_FUNC(0x4358D0)
@@ -73,16 +101,62 @@ s32 DrawUnk_0xBC::sub_435B90()
 	return 0;
 }
 
+s32 dword_67671C;
+s32 dword_67681C;
+
 STUB_FUNC(0x435D20)
-s32 DrawUnk_0xBC::sub_435D20(char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7)
+void DrawUnk_0xBC::sub_435D20(char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7)
 {
-	return 0;
+	field_10 = field_0;
+	field_14 = field_4_unk;
+	field_18 = field_8;
+	field_1C = field_C;
+
+	if (a2)
+	{
+		field_14 -= dword_67671C;
+	}
+
+	if (a3)
+	{
+		field_14 += dword_67671C;
+	}
+
+	if (a4)
+	{
+		field_10 -= dword_67671C;
+	}
+
+	if (a5)
+	{
+		field_10 += dword_67671C;
+	}
+
+	if (a6)
+	{
+		field_18 += dword_67681C;
+	}
+
+	if (a7)
+	{
+		field_18 -= dword_67681C;
+	}
+
+	sub_435840();
 }
 
-STUB_FUNC(0x435DD0)
-s32 DrawUnk_0xBC::sub_435DD0()
+MATCH_FUNC(0x435DD0)
+void DrawUnk_0xBC::sub_435DD0()
 {
-	return 0;
+	field_98_x = field_0;
+	field_9C_y = field_4_unk;
+	field_A0_z = field_8;
+	field_A4 = field_C;
+
+	field_AC = dword_676818;
+	field_B0 = dword_676818;
+	field_B4 = dword_676818;
+	field_B8 = dword_676818;
 }
 
 STUB_FUNC(0x435F90)
--- Source/DrawUnk_0xBC.hpp
+++ Source/DrawUnk_0xBC.hpp
@@ -1,26 +1,29 @@
 #pragma once
 
 #include "types.hpp"
+#include "Function.hpp"
 
 // TODO: add later
 class Car_3C;
 
 class DrawUnk_0xBC
 {
+public:
+
 	EXPORT char_type sub_4355D0(Car_3C* a2);
 	EXPORT char_type sub_435630(s16* a2, s32 a3);
-	EXPORT s32 sub_4357B0();
-	EXPORT s32 sub_4357F0();
-	EXPORT s32 sub_435810();
-	EXPORT s32 sub_435830();
-	EXPORT s32 sub_435840();
-	EXPORT s32 sub_435860(DrawUnk_0xBC* a2);
+	EXPORT void sub_4357B0();
+	EXPORT void sub_4357F0();
+	EXPORT void sub_435810();
+	EXPORT void sub_435830();
+	EXPORT void sub_435840();
+	EXPORT void sub_435860(DrawUnk_0xBC* a2);
 	EXPORT s16* sub_4358D0(s16* a2);
 	EXPORT s32* sub_435A20(s32* a2);
 	EXPORT s32 sub_435A70(s32 a2, s32 a3, s32 a4);
 	EXPORT s32 sub_435B90();
-	EXPORT s32 sub_435D20( char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7);
-	EXPORT s32 sub_435DD0();
+	EXPORT void sub_435D20( char_type a2, char_type a3, char_type a4, char_type a5, char_type a6, char_type a7);
+	EXPORT void sub_435DD0();
 	EXPORT u8 sub_435F90(s32 a2);
 	EXPORT s32 sub_435FF0();
 	EXPORT s32 sub_436110();
@@ -42,8 +45,7 @@ class DrawUnk_0xBC
 	s32 field_0;
 	s32 field_4_unk;
 	s32 field_8;
-	s16 field_C;
-	s16 field_E;
+	s32 field_C;
 	s32 field_10;
 	s32 field_14;
 	s32 field_18;
@@ -91,4 +93,4 @@ class DrawUnk_0xBC
 	s32 field_B4;
 	s32 field_B8;
 };
-
+GTA2_ASSERT_SIZEOF_ALWAYS(DrawUnk_0xBC, 0xBC)
