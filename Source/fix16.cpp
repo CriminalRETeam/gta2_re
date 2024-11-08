5773a0cb0da58ee961d82305dd79e10e82ab3c9d
Paul <github@paulsapps.com>
2024-11-08 02:47:42 +0000

    match Fix16::operator*

--- Source/fix16.cpp
+++ Source/fix16.cpp
@@ -2,15 +2,23 @@
 #include "fix16.hpp"
 
 MATCH_FUNC(0x4369F0)
-Fix16& Fix16::FromInt_4369F0(s32 a2)
+Fix16 &Fix16::FromInt_4369F0(s32 a2)
 {
     mValue = a2 << 14;
     return *this;
 }
 
 MATCH_FUNC(0x45C4E0)
-Fix16& Fix16::FromInt_45C4E0(u8 a2)
+Fix16 &Fix16::FromInt_45C4E0(u8 a2)
 {
     mValue = a2 << 14;
     return *this;
+}
+
+MATCH_FUNC(0x408680)
+Fix16 Fix16::operator*(const Fix16 &in)
+{
+    Fix16 ret;
+    ret.mValue = (mValue * (__int64)in.mValue) >> 14;
+    return ret;
 }
