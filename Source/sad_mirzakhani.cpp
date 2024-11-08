a9ef0c28486df1751cb80acaef243df792539c33
Paul <github@paulsapps.com>
2024-11-08 20:58:22 +0000

    split out sad_mirzakhani

---
+++ Source/sad_mirzakhani.cpp
@@ -0,0 +1,8 @@
+#include "sad_mirzakhani.hpp"
+#include "Function.hpp"
+
+MATCH_FUNC(0x431DB0);
+void silly_saha_0x2C::sub_431DB0()
+{
+    field_2B = 0;
+}
---
+++ Source/sad_mirzakhani.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "types.hpp"
+
+// todo: move
+class silly_saha_0x2C
+{
+public:
+
+    void sub_431DB0();
+
+    s32 field_0;
+    s16 field_4;
+    char_type field_6;
+    char_type field_7;
+    s32 field_8;
+    s32 field_C;
+    s16 field_10;
+    s16 field_12;
+    s32 field_14;
+    s32 field_18;
+    s32 field_1C;
+    s32 field_20_counterVal;
+    char_type field_24;
+    char_type field_25;
+    char_type field_26;
+    char_type field_27;
+    s16 field_28;
+    char_type field_2A_bUsed;
+    char_type field_2B;
+};
+
+class sad_mirzakhani
+{
+public:
+    silly_saha_0x2C field_0[10];
+    s32 field_1B8;
+    s32 field_1BC;
+};
