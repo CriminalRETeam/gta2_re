1c7f741ad2bef379763adee70d45db924efa0824
Paul <github@paulsapps.com>
2024-11-09 05:02:24 +0000

    match Ped_Unknown_4::sub_471680

--- Source/Ped_Unknown_4.cpp
+++ Source/Ped_Unknown_4.cpp
@@ -1,19 +1,20 @@
 #include "Ped_Unknown_4.hpp"
+#include "cool_nash_0x294.hpp"
 
 STUB_FUNC(0x471140)
-Char_8 * Ped_Unknown_4::sub_471140(cool_nash_0x294 * pPed)
+Char_8 *Ped_Unknown_4::sub_471140(cool_nash_0x294 *pPed)
 {
     return 0;
 }
 
 STUB_FUNC(0x471160)
-Char_8 * Ped_Unknown_4::sub_471160(cool_nash_0x294 * a2)
+Char_8 *Ped_Unknown_4::sub_471160(cool_nash_0x294 *a2)
 {
     return 0;
 }
 
 STUB_FUNC(0x4711f0)
-Char_8 * Ped_Unknown_4::sub_4711F0(cool_nash_0x294 * a2)
+Char_8 *Ped_Unknown_4::sub_4711F0(cool_nash_0x294 *a2)
 {
     return 0;
 }
@@ -25,33 +26,41 @@ char_type Ped_Unknown_4::sub_471290()
 }
 
 STUB_FUNC(0x471320)
-cool_nash_0x294 * Ped_Unknown_4::sub_471320()
+cool_nash_0x294 *Ped_Unknown_4::sub_471320()
 {
     return 0;
 }
 
 STUB_FUNC(0x4715a0)
-Char_8 ** Ped_Unknown_4::sub_4715A0()
+Char_8 **Ped_Unknown_4::sub_4715A0()
 {
     return 0;
 }
 
 STUB_FUNC(0x4715e0)
-Char_8 ** Ped_Unknown_4::sub_4715E0()
+Char_8 **Ped_Unknown_4::sub_4715E0()
 {
     return 0;
 }
 
 STUB_FUNC(0x471630)
-cool_nash_0x294 * Ped_Unknown_4::sub_471630()
+cool_nash_0x294 *Ped_Unknown_4::sub_471630()
 {
     return 0;
 }
 
-STUB_FUNC(0x471680)
-Char_8 * Ped_Unknown_4::sub_471680()
+MATCH_FUNC(0x471680)
+void Ped_Unknown_4::sub_471680()
 {
-    return 0;
+    Char_8 *pIter; // eax
+
+    for (pIter = this->field_0_pOwner; pIter; pIter = pIter->field_4_pOwner)
+    {
+        if (pIter->field_0_char_ped->field_240_occupation == 7)
+        {
+            pIter->field_0_char_ped->field_21C |= 0x20000000u;
+        }
+    }
 }
 
 STUB_FUNC(0x4716b0)
@@ -61,7 +70,7 @@ s32 Ped_Unknown_4::sub_4716B0()
 }
 
 STUB_FUNC(0x4716d0)
-Char_8 * Ped_Unknown_4::sub_4716D0(Car_BC * a2)
+Char_8 *Ped_Unknown_4::sub_4716D0(Car_BC *a2)
 {
     return 0;
 }
@@ -71,4 +80,3 @@ char_type Ped_Unknown_4::sub_471710()
 {
     return 0;
 }
-
--- Source/Ped_Unknown_4.hpp
+++ Source/Ped_Unknown_4.hpp
@@ -7,6 +7,14 @@ class cool_nash_0x294;
 class Char_8;
 class Car_BC;
 
+// TODO: Move
+class Char_8
+{
+public:
+  cool_nash_0x294 *field_0_char_ped;
+  Char_8 *field_4_pOwner;
+};
+
 class Ped_Unknown_4
 {
 public:
@@ -18,7 +26,7 @@ public:
     EXPORT Char_8** sub_4715A0();
     EXPORT Char_8** sub_4715E0();
     EXPORT cool_nash_0x294* sub_471630();
-    EXPORT Char_8* sub_471680();
+    EXPORT void sub_471680();
     EXPORT s32 sub_4716B0();
     EXPORT Char_8* sub_4716D0(Car_BC* a2);
     EXPORT char_type sub_471710();
