8fa900aecf1f9544ec3883417866aa197f0cf8e9
Paul <github@paulsapps.com>
2024-11-09 03:17:23 +0000

    stop on error you slag

--- compare_matches.sh
+++ compare_matches.sh
@@ -1,6 +1,6 @@
 # checks functions marked as matching actually match for a linux build
-WINEDEBUG=-all wine build.bat
+WINEDEBUG=-all wine build.bat || exit 1
 cd Scripts/bin_comp
-python msvc_dump_func_data.py
-./venv/bin/python compare_all_functions.py
+python msvc_dump_func_data.py || exit 1
+./venv/bin/python compare_all_functions.py || exit 1
 cd ../..
