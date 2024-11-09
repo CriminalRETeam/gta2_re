bbadbb39864e889051d945f621befcef5cb0491a
Paul <github@paulsapps.com>
2024-11-09 01:57:33 +0000

    match Car_10::sub_439E60

--- Source/Car_10.cpp
+++ Source/Car_10.cpp
@@ -41,10 +41,27 @@ void Car_10::sub_439E40(u8 a2)
     field_2 = byte_61A808[a2];
 }
 
-STUB_FUNC(0x439E60)
+MATCH_FUNC(0x439E60)
 s32 Car_10::sub_439E60()
 {
-    return 0;
+    const s32 result = field_4 - 1;
+    switch (field_4)
+    {
+    case 1:
+    case 2:
+        field_C = 50;
+        break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        field_1 = 4;
+        field_4 = 2;
+        break;
+    default:
+        break;
+    }
+    return result;
 }
 
 MATCH_FUNC(0x439EA0)
