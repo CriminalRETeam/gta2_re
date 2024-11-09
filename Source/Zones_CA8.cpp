2b5fcd2620a172eede2cd7a0d013a63ded00d84c
Paul <github@paulsapps.com>
2024-11-09 03:58:29 +0000

    match Map_0x370::sub_4DFB50

---
+++ Source/Zones_CA8.cpp
@@ -0,0 +1,10 @@
+#include "Zones_CA8.hpp"
+#include "Function.hpp"
+
+EXPORT_VAR Zone_144* gZones_CA8_67E274;
+
+STUB_FUNC(0x4BF100);
+Zone_144* Zone_144::zone_by_name_4BF100(const char *pZoneName)
+{
+    return 0;
+}
