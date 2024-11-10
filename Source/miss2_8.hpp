7e5bd39ae079616b0e930427e03e36475ccc1e89
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-10 04:41:33 +0100

    match miss2_8 class

--- Source/miss2_8.hpp
+++ Source/miss2_8.hpp
@@ -2,7 +2,25 @@
 
 #include "types.hpp"
 
-class Frismo_C;
+// TODO: move
+class Frismo_C
+{
+public:
+    s32 field_0;
+    s16 field_4;
+    s16 field_6;
+    Frismo_C* field_8_next;
+};
+GTA2_ASSERT_SIZEOF_ALWAYS(Frismo_C, 0xC)
+
+struct Frismo_25C
+{
+    Frismo_C * field_0;
+    Frismo_C field_4[50];
+};
+GTA2_ASSERT_SIZEOF_ALWAYS(Frismo_25C, 0x25C)
+
+extern Frismo_25C* gFrismo_25C_6F8068;
 
 class miss2_8
 {
@@ -10,13 +28,14 @@ public:
     miss2_8(); // 503120
     ~miss2_8(); // 503130
 
-    Frismo_C* sub_503160(Frismo_C* a2);
-    Frismo_C* sub_503180();
-    void sub_5031E0(u8 a2);
+    void add_503160(Frismo_C* a2);
+    Frismo_C* remove_503180();
+    void remove_5031E0(u8 count);
 
-    Frismo_C* field_0;
-    char_type field_4;
-    char_type field_5;
-    char_type field_6;
-    char_type field_7;
+    Frismo_C* field_0_current;
+    u8 field_4_count;
+    u8 field_5_pad;
+    u8 field_6_pad;
+    u8 field_7_pad;
 };
+GTA2_ASSERT_SIZEOF_ALWAYS(miss2_8, 0x8)
