4b12fa45636aeda92411d9d15da1c7111cc25048
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 12:08:52 +0100

    stub Collide_C, Phi_74 and Phi_8CA8

---
+++ Source/Phi_8CA8.cpp
@@ -0,0 +1,140 @@
+#include "Phi_8CA8.hpp"
+
+STUB_FUNC(0x4bdf60)
+void Phi_74::dtor_4BDF60()
+{
+
+}
+
+STUB_FUNC(0x533060)
+s32 Phi_74::sub_533060(s32 a2, s32 a3, s32 a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x533090)
+s32 Phi_74::sub_533090()
+{
+    return 0;
+}
+
+STUB_FUNC(0x533110)
+void Phi_74::sub_533110(s16 a2)
+{
+
+}
+
+STUB_FUNC(0x533150)
+s16 Phi_74::sub_533150(s16 a2, char_type a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x533170)
+Car_3C* Phi_74::sub_533170()
+{
+    return 0;
+}
+
+STUB_FUNC(0x5331a0)
+s32 Phi_74::sub_5331A0(Car_3C* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x533220)
+void Phi_74::ctor_533220()
+{
+
+}
+
+
+STUB_FUNC(0x4bdf40)
+void Phi_8CA8::dtor_4BDF40()
+{
+
+}
+
+STUB_FUNC(0x5332d0)
+Phi_74* Phi_8CA8::sub_5332D0(s32 a2, s32 a3, s16 a4, char_type a5)
+{
+    return 0;
+}
+
+STUB_FUNC(0x533300)
+object_info* Phi_8CA8::sub_533300()
+{
+    return 0;
+}
+
+STUB_FUNC(0x533360)
+void Phi_8CA8::sub_533360()
+{
+
+}
+
+STUB_FUNC(0x533420)
+Phi_74* Phi_8CA8::sub_533420()
+{
+    return 0;
+}
+
+STUB_FUNC(0x533b30)
+s32 Phi_8CA8::sub_533B30()
+{
+    return 0;
+}
+
+STUB_FUNC(0x533c90)
+u8 Phi_8CA8::sub_533C90()
+{
+    return 0;
+}
+
+STUB_FUNC(0x534270)
+u8 Phi_8CA8::sub_534270()
+{
+    return 0;
+}
+
+STUB_FUNC(0x5342d0)
+void Phi_8CA8::sub_5342D0()
+{
+
+}
+
+STUB_FUNC(0x5342f0)
+u8 Phi_8CA8::sub_5342F0(s32 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x534330)
+void Phi_8CA8::sub_534330()
+{
+
+}
+
+STUB_FUNC(0x534360)
+Phi_74* Phi_8CA8::sub_534360(s32 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x534370)
+Phi_74* Phi_8CA8::sub_534370(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x5343c0)
+Phi_74* Phi_8CA8::sub_5343C0(s32 idx)
+{
+    return 0;
+}
+
+STUB_FUNC(0x5343f0)
+Phi_8CA8* Phi_8CA8::ctor_5343F0()
+{
+    return 0;
+}
