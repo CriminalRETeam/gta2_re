067056ae32e87a001c7614bf2ae21834a86dd656
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 11:34:28 +0100

    stub Frismo_25C and Frismo_C

---
+++ Source/Frismo_25C.cpp
@@ -0,0 +1,26 @@
+#include "Frismo_25C.hpp"
+
+STUB_FUNC(0x4bea80)
+Frismo_C::Frismo_C()
+{
+
+}
+
+STUB_FUNC(0x4beaa0)
+Frismo_C::~Frismo_C()
+{
+
+}
+
+STUB_FUNC(0x503110)
+s32 Frismo_C::sub_503110()
+{
+    return 0;
+}
+
+
+STUB_FUNC(0x4bc300)
+Frismo_25C::~Frismo_25C()
+{
+
+}
