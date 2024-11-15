006873da1edcf4954dcffef74235bc7842f5fbeb
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 14:05:36 +0100

    stub Shooey_CC, Shooey_14

---
+++ Source/Shooey_CC.cpp
@@ -0,0 +1,92 @@
+#include "Shooey_CC.hpp"
+
+STUB_FUNC(0x484cb0)
+void Shooey_14::ctor_484CB0()
+{
+
+}
+
+STUB_FUNC(0x484ce0)
+void Shooey_14::dtor_484CE0()
+{
+
+}
+
+STUB_FUNC(0x484cf0)
+s32 Shooey_14::sub_484CF0(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x484d50)
+s32 Shooey_14::sub_484D50(s32* a2, s32* a3, s32* a4, u32* a5)
+{
+    return 0;
+}
+
+
+STUB_FUNC(0x484d80)
+Shooey_CC* Shooey_CC::ctor_484D80()
+{
+    return 0;
+}
+
+STUB_FUNC(0x484db0)
+void Shooey_CC::dtor_484DB0()
+{
+
+}
+
+STUB_FUNC(0x484dd0)
+void Shooey_CC::sub_484DD0(s32 a2, s32 a3)
+{
+
+}
+
+STUB_FUNC(0x484e20)
+bool Shooey_CC::sub_484E20(s32* a2, s32* a3, s32* a4, u32* a5)
+{
+    return 0;
+}
+
+STUB_FUNC(0x484e90)
+char_type Shooey_CC::sub_484E90(s32 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x484fc0)
+Shooey_CC* Shooey_CC::ctor_484FC0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x484fd0)
+void Shooey_CC::dtor_484FD0()
+{
+
+}
+
+STUB_FUNC(0x484fe0)
+void Shooey_CC::sub_484FE0(u32 a2, cool_nash_0x294* pPed)
+{
+
+}
+
+STUB_FUNC(0x485090)
+bool Shooey_CC::sub_485090(Car_BC* a2, angry_lewin_0x85C* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4850f0)
+char_type Shooey_CC::sub_4850F0(Shooey_CC* a1, s32 a2, angry_lewin_0x85C* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x485140)
+char_type Shooey_CC::sub_485140(cool_nash_0x294* a2, angry_lewin_0x85C* a3)
+{
+    return 0;
+}
