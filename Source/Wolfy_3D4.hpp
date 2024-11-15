ad35fe1280d494cd14457107bd8026f717929245
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 13:25:53 +0100

    stub Wolfy_3D4, Wolfy_7A8, Wolfy_30

---
+++ Source/Wolfy_3D4.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Function.hpp"
+#include "types.hpp"
+
+class Wolfy_30
+{
+public:
+    EXPORT void ctor_5408F0();
+    EXPORT void dtor_540A10();
+    EXPORT s32 sub_543650();
+    s32 field_0;
+    char_type field_4_idx;
+    char_type field_5;
+    s16 field_6;
+    s32 field_8;
+    s16 field_C;
+    s16 field_E;
+    s32 field_10;
+    s32 field_14;
+    s16 field_18;
+    s16 field_1A;
+    s32 field_1C;
+    s16 field_20;
+    s16 field_22;
+    s32 field_24;
+    s32 field_28;
+    s32 field_2C;
+};
+
+class Wolfy_7A8
+{
+public:
+    EXPORT s32 sub_543690();
+    EXPORT Wolfy_30* sub_543800();
+    EXPORT Wolfy_7A8* ctor_5438B0();
+    EXPORT void dtor_5438F0();
+    Wolfy_30 field_0[40];
+    char_type field_780[40];
+};
+
+class Wolfy_3D4
+{
+public:
+    EXPORT Wolfy_3D4* ctor_543980();
+    EXPORT void dtor_5439C0();
+    Wolfy_30 field_0[20];
+    char_type field_3C0[20];
+};
