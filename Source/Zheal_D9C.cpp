6e3f722057c67d0f274f6cc2a8b3ade64e2d3102
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 13:30:16 +0100

    stub Zheal_D9C, Zheal_15C, Snooky_30, Snooky_94

---
+++ Source/Zheal_D9C.cpp
@@ -0,0 +1,218 @@
+#include "Zheal_D9C.hpp"
+
+STUB_FUNC(0x47e5b0)
+void Zheal_15C::dtor_47E5B0()
+{
+
+}
+
+STUB_FUNC(0x47e610)
+void Zheal_15C::ctor_47E610()
+{
+
+}
+
+STUB_FUNC(0x47e620)
+s32 Zheal_15C::sub_47E620(s32 a2, s32 a3, s32* a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47e730)
+s32 Zheal_15C::sub_47E730(s32 a2, s32 a3, u32 a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47e840)
+s32 Zheal_15C::sub_47E840(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47e920)
+bool Zheal_15C::sub_47E920()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47eb00)
+bool Zheal_15C::sub_47EB00()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47ecc0)
+s32 Zheal_15C::sub_47ECC0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47ed60)
+s32 Zheal_15C::sub_47ED60()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47edf0)
+s32 Zheal_15C::sub_47EDF0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47ef80)
+s32 Zheal_15C::sub_47EF80()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f170)
+s32 Zheal_15C::sub_47F170()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f220)
+s32 Zheal_15C::sub_47F220(s32 a2, s32 a3, Car_3C* a4, s32 a5)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f290)
+s32 Zheal_15C::sub_47F290(s32 a2, s32 a3, Car_3C* a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f2f0)
+s32 Zheal_15C::sub_47F2F0(s32 a2, s32 a3, u32* a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f350)
+bool Zheal_15C::sub_47F350()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f3d0)
+bool Zheal_15C::sub_47F3D0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f450)
+bool Zheal_15C::sub_47F450()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f4c0)
+void Zheal_15C::sub_47F4C0()
+{
+
+}
+
+STUB_FUNC(0x47f6c0)
+char_type Zheal_15C::sub_47F6C0(u32* a2, s32* a3, u32* a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f7f0)
+char_type Zheal_15C::sub_47F7F0(u32* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x47f930)
+void Zheal_15C::sub_47F930(s32 a2)
+{
+
+}
+
+STUB_FUNC(0x47fb40)
+s32 Zheal_15C::sub_47FB40()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47fba0)
+s32 Zheal_15C::sub_47FBA0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47fd10)
+s32 Zheal_15C::sub_47FD10()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47fd50)
+char_type Zheal_15C::sub_47FD50()
+{
+    return 0;
+}
+
+STUB_FUNC(0x47fe10)
+void Zheal_15C::sub_47FE10()
+{
+
+}
+
+STUB_FUNC(0x480310)
+void Zheal_15C::sub_480310()
+{
+
+}
+
+STUB_FUNC(0x4803b0)
+infallible_turing* Zheal_15C::sub_4803B0(s32 a2, s32 a3, char_type a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x480900)
+s32* Zheal_15C::sub_480900(s32 a2, s32 a3, s32 a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x480b60)
+s32* Zheal_15C::sub_480B60(s32 a2, s32 a3, s32 a4)
+{
+    return 0;
+}
+
+
+STUB_FUNC(0x480e00)
+void Zheal_D9C::sub_480E00(Car_BC* a2, u8 a3)
+{
+
+}
+
+STUB_FUNC(0x480e50)
+void Zheal_D9C::sub_480E50()
+{
+
+}
+
+STUB_FUNC(0x480ec0)
+Zheal_15C* Zheal_D9C::sub_480EC0(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x480f50)
+Zheal_D9C* Zheal_D9C::ctor_480F50()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4bbbf0)
+void Zheal_D9C::dtor_4BBBF0()
+{
+
+}
---
+++ Source/Zheal_D9C.hpp
@@ -0,0 +1,156 @@
+#pragma once
+
+#include "Function.hpp"
+#include "types.hpp"
+
+class infallible_turing;
+class Car_3C;
+class Car_BC;
+
+class Zheal_15C
+{
+public:
+    EXPORT void dtor_47E5B0();
+    EXPORT void ctor_47E610();
+    EXPORT s32 sub_47E620(s32 a2, s32 a3, s32* a4);
+    EXPORT s32 sub_47E730(s32 a2, s32 a3, u32 a4);
+    EXPORT s32 sub_47E840(s32 a2, s32 a3);
+    EXPORT bool sub_47E920();
+    EXPORT bool sub_47EB00();
+    EXPORT s32 sub_47ECC0();
+    EXPORT s32 sub_47ED60();
+    EXPORT s32 sub_47EDF0();
+    EXPORT s32 sub_47EF80();
+    EXPORT s32 sub_47F170();
+    EXPORT s32 sub_47F220(s32 a2, s32 a3, Car_3C* a4, s32 a5);
+    EXPORT s32 sub_47F290(s32 a2, s32 a3, Car_3C* a4);
+    EXPORT s32 sub_47F2F0(s32 a2, s32 a3, u32* a4);
+    EXPORT bool sub_47F350();
+    EXPORT bool sub_47F3D0();
+    EXPORT bool sub_47F450();
+    EXPORT void sub_47F4C0();
+    EXPORT char_type sub_47F6C0(u32* a2, s32* a3, u32* a4);
+    EXPORT char_type sub_47F7F0(u32* a2);
+    EXPORT void sub_47F930(s32 a2);
+    EXPORT s32 sub_47FB40();
+    EXPORT s32 sub_47FBA0();
+    EXPORT s32 sub_47FD10();
+    EXPORT char_type sub_47FD50();
+    EXPORT void sub_47FE10();
+    EXPORT void sub_480310();
+    EXPORT infallible_turing* sub_4803B0(s32 a2, s32 a3, char_type a4);
+    EXPORT s32* sub_480900(s32 a2, s32 a3, s32 a4);
+    EXPORT s32* sub_480B60(s32 a2, s32 a3, s32 a4);
+
+    s32 field_0;
+    s32 field_4;
+    s32 field_8;
+    s32 field_C;
+    s32 field_10;
+    s32 field_14;
+    s32 field_18;
+    s32 field_1C;
+    s32 field_20;
+    s32 field_24;
+    s32 field_28;
+    s32 field_2C;
+    s32 field_30;
+    s32 field_34;
+    s32 field_38;
+    s32 field_3C;
+    s32 field_40;
+    s32 field_44;
+    s32 field_48;
+    s32 field_4C;
+    s32 field_50;
+    s32 field_54;
+    s32 field_58;
+    s32 field_5C;
+    Car_3C* field_60;
+    Car_3C* field_64;
+    Car_3C* field_68;
+    Car_3C* field_6C;
+    s32 field_70;
+    Car_3C* field_74;
+    s32 field_78;
+    infallible_turing* field_7C;
+    s32 field_80;
+    s32 field_84;
+    s32 field_88;
+    s32 field_8C;
+    s32 field_90;
+    s32 field_94;
+    s32 field_98;
+    s32 field_9C;
+    s32 field_A0;
+    s32 field_A4;
+    s32 field_A8;
+    s32 field_AC;
+    s32 field_B0;
+    s32 field_B4;
+    s32 field_B8;
+    s32 field_BC;
+    s32 field_C0;
+    s32 field_C4;
+    s32 field_C8;
+    s32 field_CC;
+    s32 field_D0;
+    s32 field_D4;
+    s32 field_D8;
+    s32 field_DC;
+    s32 field_E0;
+    s32 field_E4;
+    s32 field_E8;
+    s32 field_EC;
+    s32 field_F0;
+    s32 field_F4;
+    s32 field_F8;
+    s32 field_FC;
+    s32 field_100;
+    s32 field_104;
+    s32 field_108;
+    s32 field_10C;
+    s32 field_110;
+    s32 field_114;
+    s32 field_118;
+    s32 field_11C;
+    s32 field_120;
+    s32 field_124;
+    s32 field_128;
+    s32 field_12C;
+    s32 field_130;
+    s32 field_134;
+    s32 field_138;
+    s32 field_13C;
+    s16 field_140;
+    s16 field_142;
+    s32 field_144;
+    s32 field_148;
+    char_type field_14C;
+    char_type field_14D;
+    char_type field_14E;
+    char_type field_14F;
+    s32 field_150;
+    char_type field_154;
+    char_type field_155;
+    char_type field_156;
+    char_type field_157;
+    char_type field_158;
+    char_type field_159;
+    char_type field_15A;
+    char_type field_15B;
+};
+
+class Zheal_D9C
+{
+public:
+    EXPORT void sub_480E00(Car_BC* a2, u8 a3);
+    EXPORT void sub_480E50();
+    EXPORT Zheal_15C* sub_480EC0(s32 a2, s32 a3);
+    EXPORT Zheal_D9C* ctor_480F50();
+    EXPORT void dtor_4BBBF0();
+
+    Zheal_15C field_0[10];
+    s32 field_D98_count;
+};
+
