c14f1374abd5b8e80f1300925f15c5bbe91d3ff3
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 14:03:22 +0100

    stub Hamburger_40, Hamburger_500, CokeZero_50, CokeZero_100

---
+++ Source/Hamburger_500.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "Function.hpp"
+#include "types.hpp"
+
+class cool_nash_0x294;
+
+class Hamburger_40
+{
+public:
+    EXPORT s32 sub_4747B0();
+    EXPORT void ctor_474CA0();
+    EXPORT void dtor_474CB0();
+
+    char_type field_0;
+    char_type field_1;
+    char_type field_2;
+    char_type field_3;
+    s32 field_4;
+    s32 field_8;
+    s32 field_C;
+    char_type field_10;
+    char_type field_11;
+    char_type field_12;
+    char_type field_13;
+    s32 field_14;
+    s32 field_18;
+    s32 field_1C;
+    char_type field_20;
+    char_type field_21;
+    char_type field_22;
+    char_type field_23;
+    char_type field_24;
+    char_type field_25;
+    char_type field_26;
+    char_type field_27;
+    char_type field_28;
+    char_type field_29;
+    s16 field_2A;
+    u16 field_2C;
+    s16 field_2E;
+    s32 field_30;
+    s16 field_34;
+    char_type field_36;
+    char_type field_37;
+    s32 field_38;
+    char_type field_3C;
+    char_type field_3D;
+    char_type field_3E;
+    char_type field_3F;
+};
+
+class Hamburger_500
+{
+public:
+    EXPORT Hamburger_40* sub_474810();
+    EXPORT char_type sub_474850(cool_nash_0x294* a1, s32 a2);
+    EXPORT s32 sub_4748A0(s32 a2, cool_nash_0x294* a3);
+    EXPORT char_type sub_474920(s32 a2, cool_nash_0x294* a3);
+    EXPORT char_type sub_474970(s32 a2);
+    EXPORT char_type sub_4749B0(cool_nash_0x294* a1);
+    EXPORT char_type sub_474A20(cool_nash_0x294* a1);
+    EXPORT char_type sub_474A80(cool_nash_0x294* a1);
+    EXPORT char_type sub_474AF0(cool_nash_0x294* a1);
+    EXPORT char_type sub_474B50(cool_nash_0x294* a1);
+    EXPORT char_type sub_474BC0(cool_nash_0x294* a1);
+    EXPORT char_type sub_474C30(cool_nash_0x294* a1);
+    EXPORT void sub_474CC0(Hamburger_40* a2);
+    EXPORT Hamburger_500* ctor_474D10();
+    EXPORT void dtor_474D30();
+
+    Hamburger_40 field_0[20];
+};
