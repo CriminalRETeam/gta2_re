47fa11475cb18dba673de5bc421ea32f4aa303ef
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-15 12:46:52 +0100

    stub jawwie_110, jawwie_20, jawwie_8

---
+++ Source/jawwie_110.cpp
@@ -0,0 +1,92 @@
+#include "jawwie_110.hpp"
+
+STUB_FUNC(0x4beab0)
+void jawwie_20::ctor_4BEAB0()
+{
+
+}
+
+STUB_FUNC(0x4bead0)
+void jawwie_20::dtor_4BEAD0()
+{
+
+}
+
+STUB_FUNC(0x4fa7d0)
+s32 jawwie_20::sub_4FA7D0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa800)
+void jawwie_20::sub_4FA800(cool_nash_0x294* pPed)
+{
+
+}
+
+STUB_FUNC(0x4fa820)
+char_type jawwie_20::sub_4FA820()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa9d0)
+char_type jawwie_20::sub_4FA9D0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4faac0)
+u32* jawwie_20::sub_4FAAC0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fb330)
+void jawwie_20::sub_4FB330()
+{
+
+}
+
+
+STUB_FUNC(0x4beae0)
+void jawwie_110::dtor_4BEAE0()
+{
+
+}
+
+STUB_FUNC(0x4fa310)
+jawwie_8* jawwie_110::ctor_4FA310()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa330)
+char_type jawwie_110::sub_4FA330(cool_nash_0x294* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa470)
+char_type jawwie_110::sub_4FA470(cool_nash_0x294* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa4b0)
+jawwie_20* jawwie_110::sub_4FA4B0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4fa500)
+void jawwie_110::sub_4FA500()
+{
+
+}
+
+STUB_FUNC(0x4fa790)
+void jawwie_110::sub_4FA790()
+{
+
+}
