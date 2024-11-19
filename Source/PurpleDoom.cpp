273db39b4027b3070775fa3eca96517f71f6df1a
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-19 13:51:13 +0100

    stub PurpleDoom

---
+++ Source/PurpleDoom.cpp
@@ -0,0 +1,163 @@
+#include "PurpleDoom.hpp"
+
+STUB_FUNC(0x477a40)
+s32 PurpleDoom::sub_477A40()
+{
+    return 0;
+}
+
+STUB_FUNC(0x477ae0)
+char_type PurpleDoom::sub_477AE0(Car_3C* a1)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477b00)
+void PurpleDoom::sub_477B00(Car_3C* a1)
+{
+
+}
+
+STUB_FUNC(0x477b20)
+s32 PurpleDoom::sub_477B20(Car_3C* a1a)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477b60)
+s32 PurpleDoom::sub_477B60(Car_3C* a1)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477ba0)
+void PurpleDoom::sub_477BA0()
+{
+
+}
+
+STUB_FUNC(0x477bd0)
+char_type PurpleDoom::sub_477BD0(Car_3C* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477c30)
+char_type PurpleDoom::sub_477C30(Car_3C* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477c90)
+s32 PurpleDoom::sub_477C90(s32 a1, s32 a2, s32* a3, u8 a4, s32 a5, char_type a6)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477e60)
+s32 PurpleDoom::sub_477E60(Car_3C* a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477f30)
+char_type PurpleDoom::sub_477F30(s32* a2, char_type a3, s32 a4, Car_3C* a5, Object_3C* a6)
+{
+    return 0;
+}
+
+STUB_FUNC(0x477f60)
+char_type PurpleDoom::sub_477F60(s32* a2, char_type a3, s32 a4, Car_3C* a5)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478040)
+PurpleDoom* PurpleDoom::ctor_478040()
+{
+    return 0;
+}
+
+STUB_FUNC(0x478050)
+s32 PurpleDoom::dtor_478050()
+{
+    return 0;
+}
+
+STUB_FUNC(0x478160)
+u32 PurpleDoom::sub_478160(u8 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478240)
+s32 PurpleDoom::sub_478240(s32 a2, s32 a3, s32 a4, s32 a5)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4782c0)
+void PurpleDoom::sub_4782C0(s32 a2, s32 idx, Car_3C* a4)
+{
+
+}
+
+STUB_FUNC(0x478370)
+u8* PurpleDoom::sub_478370(s32 a2, s32 a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478440)
+char_type PurpleDoom::sub_478440(s32 a2, s32 idx, Car_3C* a4)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4784d0)
+void PurpleDoom::sub_4784D0(s32 idx, Car_3C* a3)
+{
+
+}
+
+STUB_FUNC(0x478590)
+u8* PurpleDoom::sub_478590(u32 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4785d0)
+char_type PurpleDoom::sub_4785D0(u32 a2, s32* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478750)
+char_type PurpleDoom::sub_478750(u32 a2, u32* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4787e0)
+char_type PurpleDoom::sub_4787E0(u32 a2, u32* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478880)
+s32 PurpleDoom::sub_478880(u32 a2, u32* a3)
+{
+    return 0;
+}
+
+STUB_FUNC(0x478950)
+void PurpleDoom::sub_478950(s32 a2, s32 a3)
+{
+
+}
+
+STUB_FUNC(0x4789f0)
+PurpleDoom* PurpleDoom::sub_4789F0()
+{
+    return 0;
+}
