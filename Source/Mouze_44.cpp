fa5c7a497be39fd6b9438380fef438039e7eadd1
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-11-24 14:51:56 +0100

    stub Mouze_44

---
+++ Source/Mouze_44.cpp
@@ -0,0 +1,213 @@
+#include "Mouze_44.hpp"
+
+STUB_FUNC(0x4c8e60)
+void Mouze_44::sub_4C8E60()
+{
+}
+
+STUB_FUNC(0x4c8e80)
+void Mouze_44::sub_4C8E80()
+{
+}
+
+STUB_FUNC(0x4c8e90)
+cool_nash_0x294** Mouze_44::sub_4C8E90()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c8ef0)
+s32 Mouze_44::sub_4C8EF0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c8f20)
+void Mouze_44::sub_4C8F20()
+{
+}
+
+STUB_FUNC(0x4c8f90)
+char_type Mouze_44::sub_4C8F90(cool_nash_0x294* pPed)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c8fe0)
+void Mouze_44::sub_4C8FE0(cool_nash_0x294* pPed)
+{
+}
+
+STUB_FUNC(0x4c9040)
+bool Mouze_44::sub_4C9040()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9150)
+char_type Mouze_44::sub_4C9150()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c91b0)
+u8 Mouze_44::sub_4C91B0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9210)
+bool Mouze_44::sub_4C9210()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9220)
+bool Mouze_44::sub_4C9220()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9240)
+void Mouze_44::sub_4C9240()
+{
+}
+
+STUB_FUNC(0x4c92a0)
+cool_nash_0x294** Mouze_44::sub_4C92A0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c93a0)
+void Mouze_44::sub_4C93A0()
+{
+}
+
+STUB_FUNC(0x4c94e0)
+void Mouze_44::sub_4C94E0(cool_nash_0x294* a2)
+{
+}
+
+STUB_FUNC(0x4c9680)
+void Mouze_44::sub_4C9680(u8 a2)
+{
+}
+
+STUB_FUNC(0x4c9970)
+void Mouze_44::sub_4C9970(cool_nash_0x294* a2)
+{
+}
+
+STUB_FUNC(0x4c9b10)
+cool_nash_0x294* Mouze_44::sub_4C9B10(cool_nash_0x294* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9b30)
+cool_nash_0x294* Mouze_44::sub_4C9B30(cool_nash_0x294* ptr, u8 idx)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9b60)
+char_type Mouze_44::sub_4C9B60(cool_nash_0x294* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9ed0)
+cool_nash_0x294* Mouze_44::sub_4C9ED0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4c9f00)
+void Mouze_44::sub_4C9F00()
+{
+}
+
+STUB_FUNC(0x4ca3e0)
+u32 Mouze_44::sub_4CA3E0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4ca3f0)
+cool_nash_0x294* Mouze_44::sub_4CA3F0(u32* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4ca4b0)
+void Mouze_44::sub_4CA4B0()
+{
+}
+
+STUB_FUNC(0x4ca5e0)
+void Mouze_44::sub_4CA5E0(u8 idx)
+{
+}
+
+STUB_FUNC(0x4ca820)
+void Mouze_44::sub_4CA820(s32 a2)
+{
+}
+
+STUB_FUNC(0x4caa20)
+bool Mouze_44::sub_4CAA20()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4caae0)
+char_type Mouze_44::sub_4CAAE0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cab80)
+char_type Mouze_44::sub_4CAB80()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cac20)
+bool Mouze_44::sub_4CAC20(s32 idx)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cad40)
+bool Mouze_44::sub_4CAD40()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cae80)
+cool_nash_0x294* Mouze_44::sub_4CAE80(u8 idx)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cb080)
+void Mouze_44::sub_4CB080()
+{
+}
+
+STUB_FUNC(0x4cb0d0)
+Mouze_44* Mouze_44::sub_4CB0D0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x4cb860)
+void Mouze_44::ctor_4CB860()
+{
+}
+
+STUB_FUNC(0x4cb870)
+void Mouze_44::dtor_4CB870()
+{
+}
