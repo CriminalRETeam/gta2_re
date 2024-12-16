c102fe2c2ae74ec209ed55d3d6fb19139105c530
Paul <github@paulsapps.com>
2024-12-16 23:51:39 +0000

    match Snooky_94::sub_488820

--- Source/Snooky_94.cpp
+++ Source/Snooky_94.cpp
@@ -70,10 +70,13 @@ void Snooky_94::sub_4887F0()
     }
 }
 
-STUB_FUNC(0x488820)
+MATCH_FUNC(0x488820)
 Snooky_30* Snooky_94::sub_488820(s32 a2, s32 a3)
 {
-    return 0;
+    Snooky_30* pNew = &field_0[field_90_count];
+    pNew->sub_4885A0(a2, a3, field_90_count);
+    field_90_count++;
+    return pNew;
 }
 
 MATCH_FUNC(0x488860)
