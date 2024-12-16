707054e11d53e7b7cfccb5479935ff9a6bd1e87c
Paul <github@paulsapps.com>
2024-12-16 23:07:47 +0000

    match Weapon_8::Weapon_8 + Weapon_30::Weapon_30

--- Source/Weapon_8.cpp
+++ Source/Weapon_8.cpp
@@ -1,4 +1,9 @@
 #include "Weapon_8.hpp"
+#include "Globals.hpp"
+#include "error.hpp"
+
+EXPORT_VAR Weapon_2FDC* gWeapon_2FDC_707014;
+GLOBAL(gWeapon_2FDC_707014, 0x707014);
 
 STUB_FUNC(0x5e3c10)
 Weapon_30* Weapon_8::sub_5E3C10(s32 a2, cool_nash_0x294* a3, u8 a4)
@@ -47,12 +52,47 @@ char_type Weapon_8::sub_5E3E80(s32 a2)
     return 0;
 }
 
-STUB_FUNC(0x5e3e90)
+MATCH_FUNC(0x5e3e90)
 Weapon_8::Weapon_8()
 {
+    if (!gWeapon_2FDC_707014)
+    {
+        gWeapon_2FDC_707014 = new Weapon_2FDC();
+        if (!gWeapon_2FDC_707014)
+        {
+            FatalError_4A38C0(0x20, "weapon.cpp", 2428);
+        }
+    }
+    field_4 = 0;
+    field_0.sub_4207E0();
 }
 
 STUB_FUNC(0x5e3f60)
 Weapon_8::~Weapon_8()
 {
+}
+
+MATCH_FUNC(0x5DCD10)
+Weapon_30::Weapon_30()
+{
+    this->field_0 = 0;
+    this->field_24 = 0;
+    this->field_14 = 0;
+    this->field_2 = 0;
+    this->field_4 = 0;
+    this->field_18 = 0;
+    this->field_1C = 0;
+    this->field_10 = 0;
+    this->field_8 = 0;
+    this->field_C = -1;
+    this->field_20 = 0;
+    this->field_21 = 0;
+    this->field_2C = 0;
+    this->field_28 = 0;
+}
+
+STUB_FUNC(0x5DCD50)
+Weapon_30::~Weapon_30()
+{
+
 }
