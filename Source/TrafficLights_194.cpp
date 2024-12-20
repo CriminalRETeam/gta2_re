af705c8e88c8ee1c4bb0627d81e5f169e718d96e
Paul <github@paulsapps.com>
2024-12-20 22:36:17 +0000

    match TrafficLights_194::TrafficLights_194

--- Source/TrafficLights_194.cpp
+++ Source/TrafficLights_194.cpp
@@ -4,6 +4,8 @@
 EXPORT_VAR TrafficLights_194* gTrafficLights_194_705958;
 GLOBAL(gTrafficLights_194_705958, 0x705958);
 
+u8 traffic_light_phase_timers_626840[] = {0x00, 0x64, 0x18, 0x18, 0x64, 0x18, 0x18, 0x96, 0x50, 0x00, 0x00, 0x00};
+
 STUB_FUNC(0x5c2910)
 void TrafficLights_194::sub_5C2910(u8 x, u8 y, u8 w, u8 h)
 {
@@ -24,9 +26,16 @@ void TrafficLights_194::sub_5C2AC0()
 {
 }
 
-STUB_FUNC(0x5c2b00)
+MATCH_FUNC(0x5c2b00)
 TrafficLights_194::TrafficLights_194()
 {
+    field_190_array_used_count = 0;
+    for (s32 i = 0; i < GTA2_COUNTOF(field_0_traffic_lights); i++)
+    {
+        field_0_traffic_lights[i] = 0;
+    }
+    field_192_phase = 1;
+    field_193_timer = traffic_light_phase_timers_626840[1];
 }
 
 STUB_FUNC(0x5c2b30)
