d8ef1ceb632d4665b1649c5c16239c9a3a7ae886
Paul <github@paulsapps.com>
2024-12-30 21:38:04 +0000

    some rng stubs

--- Source/rng.cpp
+++ Source/rng.cpp
@@ -8,7 +8,6 @@ GLOBAL(rng_dword_67AB34, 0x67AB34);
 EXPORT_VAR rng stru_6F6784;
 GLOBAL(stru_6F6784, 0x6F6784);
 
-
 MATCH_FUNC(0x48B900)
 void rng::sub_48B900()
 {
@@ -19,7 +18,6 @@ void rng::sub_48B900()
 STUB_FUNC(0x48B920)
 void rng::ShowCycle_48B920()
 {
-
 }
 
 MATCH_FUNC(0x4F7A40)
@@ -28,6 +26,17 @@ void rng::srand_4F7A40()
     srand(1);
 }
 
+STUB_FUNC(0x4F7AE0)
+s16 rng::get_int_4F7AE0(s16* max_rnd)
+{
+    return 0;
+}
+
+STUB_FUNC(0x4F7B70)
+u8 rng::get_uint8_4F7B70(u8* max_rnd)
+{
+    return 0;
+}
 
 MATCH_FUNC(0x4F7C00)
 int rng::rand_4F7C00()
