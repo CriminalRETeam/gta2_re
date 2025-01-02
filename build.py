bfe817a4ec866fcebfc462ef275a41dea6128a03
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-02 07:25:25 +0100

    hope it builds now

--- build.py
+++ build.py
@@ -33,33 +33,69 @@ def main():
 
     print("Build finished and verified successfully!")
 
-def get_vc6_env_cmds():
+def as_wine_path(unix_path):
+    wine_path = unix_path.replace("/", "\\")
+    wine_path = unix_path.replace("\\", "\\\\")
+    wine_path = "Z:" + wine_path
+
+    return wine_path
+
+def get_vc6_env():
     vc6_root = os.path.join(CURRENT_DIRECTORY, "3rdParty", "gta2_re_compile_tools")
+
     lib = os.path.join(vc6_root, "VC98", "Lib")
-    include = f'{os.path.join(vc6_root, "VC98", "ATL", "Include")};{os.path.join(vc6_root, "VC98", "Include")};{os.path.join(vc6_root, "VC98", "MFC", "Include")}'
-    path = f'{os.path.join(vc6_root, "cmake-3.5.0-win32-x86", "bin")};{os.path.join(vc6_root, "VC98", "bin")};{os.path.join(vc6_root, "Common", "MSDev98", "Bin")}'
+    include1 = os.path.join(vc6_root, "VC98", "ATL", "Include")
+    include2 = os.path.join(vc6_root, "VC98", "Include")
+    include3 = os.path.join(vc6_root, "VC98", "MFC", "Include")
+
+    path1 = os.path.join(vc6_root, "cmake-3.5.0-win32-x86", "bin")
+    path2 = os.path.join(vc6_root, "VC98", "bin")
+    path3 = os.path.join(vc6_root, "Common", "MSDev98", "Bin")
+
+    if platform.system() == "Windows":
+        path = f'{path1};{path2};{path3}'
+        include = f'{include1};{include2};{include3};'
+    elif platform.system() == "Linux":
+        lib = as_wine_path(lib)
+        include1 = as_wine_path(include1)
+        include2 = as_wine_path(include2)
+        include3 = as_wine_path(include3)
+
+        # escape ";"
+        path = f'{path1}\;{path2}\;{path3}'
+        include = f'{include1}\;{include2}\;{include3};'
+
 
     vc6_cmds = list()
-    vc6_cmds.append(f"set LIB={lib}")
-    vc6_cmds.append(f"set INCLUDE={include}")
-    vc6_cmds.append(f"set PATH={path};%PATH%")
+    vc6_cmds.append(lib)
+    vc6_cmds.append(include)
+    vc6_cmds.append(path)
 
     return vc6_cmds
 
 def build():
     os.makedirs(BUILD_FOLDER_NAME, exist_ok=True)
 
-    all_cmds = get_vc6_env_cmds()
-    all_cmds.extend(BUILD_CMDS)
-    build_cmds = "\n".join(all_cmds) + "\n"
+    vc6_env = get_vc6_env()
+    lib = vc6_env[0]
+    include = vc6_env[1]
+    path = vc6_env[2]
 
     if platform.system() == "Linux":
-        p1 = subprocess.Popen("wine cmd", cwd=BUILD_DIRECTORY, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        build_dir = as_wine_path(BUILD_DIRECTORY)
+        command = f"export WINEPATH={path} export LIB={lib} export INCLUDE={include} wine cmd /c \"cd {build_dir} && {CMAKE_GENERATE_JOM_CMD} && {CMAKE_BUILD_CMD}\""
+        p1 = subprocess.Popen(command, cwd=BUILD_DIRECTORY, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
     elif platform.system() == "Windows":
         p1 = subprocess.Popen("cmd", cwd=BUILD_DIRECTORY, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    p1.stdin.write(build_cmds)
-    p1.stdin.close()
+        p1.stdin.write(f"set LIB={lib}\n")
+        p1.stdin.write(f"set INCLUDE={include}\n")
+        p1.stdin.write(f"set PATH={path};%PATH%\n")
+
+        for build_cmd in BUILD_CMDS:
+            p1.stdin.write(f"{build_cmd}\n")
+
+        p1.stdin.close()
     
     while True:
         output = p1.stdout.readline()
@@ -69,6 +105,7 @@ def build():
         if output:
             print(output.strip())
 
+    sys.exit(0)
     return p1.poll()
 
 def verify():
@@ -79,8 +116,8 @@ def verify():
     elif platform.system() == "Linux":
         python = "python3"
 
-    dump_result = subprocess.run(f"{python} msvc_dump_func_data.py", cwd=BIN_COMP_DIRECTORY)
-    compare_result = subprocess.run(f"{python} compare_all_functions.py", cwd=BIN_COMP_DIRECTORY)
+    dump_result = subprocess.run(f"{python} msvc_dump_func_data.py", cwd=BIN_COMP_DIRECTORY, shell=True)
+    compare_result = subprocess.run(f"{python} compare_all_functions.py", cwd=BIN_COMP_DIRECTORY, shell=True)
 
     return dump_result.returncode == 0 and compare_result.returncode == 0
 
