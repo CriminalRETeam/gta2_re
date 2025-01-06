f62977100f36dc66c6c5a85a1a5a18af1c3f893f
Valps <valps.rn@gmail.com>
2025-01-06 13:49:25 -0300

    match Map_0x370::sub_4E4D40

--- Source/map_0x370.cpp
+++ Source/map_0x370.cpp
@@ -917,10 +917,57 @@ gmp_block_info* Map_0x370::sub_4E4CB0(s32 a2, s32 a3, s32* a4)
     return gBlockInfo0_6F5EB0;
 }
 
-STUB_FUNC(0x4E4D40)
+MATCH_FUNC(0x4E4D40)
 s32* Map_0x370::sub_4E4D40(s32* a2, s32 a3, s32 a4, s32 a5)
 {
-    return 0;
+    s32 v5;
+    s32 v6;
+    gmp_block_info* block_4DFE10;
+    char_type v9;
+    s32 v10;
+    gmp_block_info* v11;
+    s32* result;
+    char_type field_B_slope_type;
+
+    s32 v12;
+    u8 v13;
+    s32 v4;
+    s32 v14;
+
+    v5 = a5;
+    v6 = a4;
+    v4 = a3;
+    if ((a5 & 0x3FFF) == dword_6F610C || (block_4DFE10 = Map_0x370::get_block_4DFE10(a3 >> 14, a4 >> 14, a5 >> 14)) == 0 ||
+        (v9 = block_4DFE10->field_B_slope_type, (v13 = v9 & 0xFCu) <= 0) //  or it's a flat block
+        || v13 >= 0xB4 //  or it's not a ramp
+        || (v9 & 3) == 0 //  or it's a air block
+        || (v10 = v5 & 0xFFFFC000, Map_0x370::sub_4E5BF0(a3, v6, &v10), v10 > v5))
+    {
+        v14 = (v5 >> 14) - 1;
+        v11 = Map_0x370::sub_4E4CB0(v4 >> 14, v6 >> 14, &v14);
+        gBlockInfo0_6F5EB0 = v11;
+        if (!v11)
+        {
+            result = a2;
+            *a2 = dword_6F6110.mValue;
+            return result;
+        }
+        field_B_slope_type = v11->field_B_slope_type;
+
+        if ((v13 = field_B_slope_type & 0xFC) > 0 //  it's not a flat block
+            && v13 < 0xB4 && (field_B_slope_type & 3) != 0)
+        {
+            v10 = v14 << 14;
+            Map_0x370::sub_4E5BF0(a3, v6, &v10);
+            result = a2;
+            *a2 = v10;
+            return result;
+        }
+        v10 = (++v14) << 14;
+    }
+    result = a2;
+    *a2 = v10;
+    return result;
 }
 
 STUB_FUNC(0x4E4E50)
