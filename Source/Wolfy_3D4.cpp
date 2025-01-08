fe1fec13df32e332d1bb63ca35fa60f229f37096
Paul <github@paulsapps.com>
2025-01-08 21:27:48 +0000

    wip Wolfy_3D4::Wolfy_3D4

--- Source/Wolfy_3D4.cpp
+++ Source/Wolfy_3D4.cpp
@@ -1,4 +1,8 @@
 #include "Wolfy_3D4.hpp"
+#include "Globals.hpp"
+
+EXPORT_VAR s16 gParticleInstCount_6FD5F4;
+GLOBAL(gParticleInstCount_6FD5F4, 0x6FD5F4);
 
 STUB_FUNC(0x543690)
 s32 Wolfy_7A8::sub_543690()
@@ -38,9 +42,16 @@ s32 Wolfy_30::sub_543650()
     return 0;
 }
 
+// https://decomp.me/scratch/TAlwS
 STUB_FUNC(0x543980)
 Wolfy_3D4::Wolfy_3D4()
 {
+    for (s32 i = 0; i < GTA2_COUNTOF(field_3C0); i++)
+    {
+        field_0[i].field_4_idx = i;
+        field_3C0[i] = 0;
+    }
+    gParticleInstCount_6FD5F4 = 0;
 }
 
 STUB_FUNC(0x5439c0)
