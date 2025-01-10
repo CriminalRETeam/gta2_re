ba3e704993aad0c917024ffebb016c73377a1912
Valps <valps.rn@gmail.com>
2025-01-09 23:29:15 -0300

    match Map_0x370::sub_4E6510

--- Source/map_0x370.cpp
+++ Source/map_0x370.cpp
@@ -1233,10 +1233,43 @@ s32* Map_0x370::sub_4E6400(s32* a2, s32 a3, s32 a4, s32 a5)
     return a2;
 }
 
-STUB_FUNC(0x4E6510)
+MATCH_FUNC(0x4E6510)
 s32* Map_0x370::sub_4E6510(s32* a2, s32 a3, s32 a4)
 {
-    return 0;
+    s32 v4;
+    s32 v5;
+    gmp_block_info* v7;
+    u8 field_B_slope_type;
+
+    s32 v10;
+
+    u32 v6;
+    u8 v13;
+
+    v4 = a3;
+    v5 = a4;
+    v7 = Map_0x370::sub_4E62D0(a3 >> 14, a4 >> 14, &v6);
+    gBlockInfo0_6F5EB0 = v7;
+    if (!v7)
+    {
+        *a2 = 0;
+        return a2;
+    }
+    else
+    {
+        field_B_slope_type = v7->field_B_slope_type;
+        if ((v13 = field_B_slope_type & 0xFC) > 0 && v13 < 0xB4 && (field_B_slope_type & 3) != 0)
+        {
+            v10 = v6 << 14;
+            Map_0x370::sub_4E5BF0(v4, v5, &v10);
+        }
+        else
+        {
+            v10 = (v6 + 1) << 14;
+        }
+    }
+    *a2 = v10;
+    return a2;
 }
 
 STUB_FUNC(0x4E65A0)
