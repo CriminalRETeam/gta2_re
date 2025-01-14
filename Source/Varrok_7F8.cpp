b308a4913f541a19a98dddacb2789d1384b78f64
Paul <github@paulsapps.com>
2025-01-14 05:10:12 +0000

    match Varrok_7F8::Varrok_7F8

--- Source/Varrok_7F8.cpp
+++ Source/Varrok_7F8.cpp
@@ -18,9 +18,14 @@ char_type Varrok_7F8::sub_59B0D0(u8 idx)
     return 0;
 }
 
-STUB_FUNC(0x59b0f0)
+MATCH_FUNC(0x59b0f0)
 Varrok_7F8::Varrok_7F8()
 {
+    for (s32 i=0; i < GTA2_COUNTOF(field_0); i++)
+    {
+        field_0[i].field_0 = 0;
+        field_0[i].field_4 = 0;
+    }
 }
 
 STUB_FUNC(0x59b110)
