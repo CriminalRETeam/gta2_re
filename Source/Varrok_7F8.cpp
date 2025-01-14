337ee874fced8938a15b7fbfe6e94217e20307ba
Paul <github@paulsapps.com>
2025-01-14 05:22:27 +0000

    wip Varrok_7F8::sub_59B060

--- Source/Varrok_7F8.cpp
+++ Source/Varrok_7F8.cpp
@@ -3,6 +3,14 @@
 STUB_FUNC(0x59b060)
 u8 Varrok_7F8::sub_59B060(s32 a2)
 {
+    for (s32 i = 1; i < GTA2_COUNTOF(field_0); i++)
+    {
+        if (field_0[i].field_0 == 0 && field_0[i].field_4 == 0)
+        {
+            field_0[i].field_0 = a2;
+            return i;
+        }
+    }
     return 0;
 }
 
@@ -24,7 +32,7 @@ void Varrok_7F8::sub_59B0D0(u8 idx)
 MATCH_FUNC(0x59b0f0)
 Varrok_7F8::Varrok_7F8()
 {
-    for (s32 i=0; i < GTA2_COUNTOF(field_0); i++)
+    for (s32 i = 0; i < GTA2_COUNTOF(field_0); i++)
     {
         field_0[i].field_0 = 0;
         field_0[i].field_4 = 0;
