db99315f5237e995b33d9df0ad794fe7211d9bf5
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-19 15:02:03 +0100

    Object_8 stubs

---
+++ Source/Object_8.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Function.hpp"
+
+class Object_8
+{
+  public:
+    EXPORT void ctor_52AD50();
+    EXPORT void dtor_52AD70();
+    Object_8* field_0;
+    s32 field_4;
+};
