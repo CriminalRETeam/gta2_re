9115ba43ebe2314c8ac0378ce3508ae5358cf8e6
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-19 15:04:02 +0100

    Object_226C stubs

---
+++ Source/Object_226C.cpp
@@ -0,0 +1,6 @@
+#include "Object_226C.hpp"
+
+STUB_FUNC(0x52a750)
+void Object_226C::dtor_52A750()
+{
+}
---
+++ Source/Object_226C.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Object_8.hpp"
+
+class Object_226C
+{
+  public:
+    EXPORT void dtor_52A750();
+    Object_8* field_0;
+    Object_8 field_4[1101];
+};
