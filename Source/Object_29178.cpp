480387e3c7901ec4d5e10f9acb396de858326721
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-19 15:15:28 +0100

    Object_29178 stubs

---
+++ Source/Object_29178.cpp
@@ -0,0 +1,6 @@
+#include "Object_29178.hpp"
+
+STUB_FUNC(0x52a720)
+void Object_29178::dtor_52A720()
+{
+}
---
+++ Source/Object_29178.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Object_5C.hpp" // for Object_2C
+
+class Object_29178
+{
+  public:
+    EXPORT void dtor_52A720();
+
+    Object_2C* field_0;
+    Object_2C* field_4;
+    Object_2C field_8[3825];
+    s16 field_29174;
+    s16 field_29176;
+};
