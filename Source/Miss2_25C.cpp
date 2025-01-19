b29b91674f0ea2f63defd87666337f96d86b56ef
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-19 14:59:07 +0100

    Miss2_25C stubs

---
+++ Source/Miss2_25C.cpp
@@ -0,0 +1,40 @@
+#include "Miss2_25C.hpp"
+
+STUB_FUNC(0x502d90)
+Miss2_25C* Miss2_25C::ctor_502D90()
+{
+    return 0;
+}
+
+STUB_FUNC(0x502dc0)
+s32 Miss2_25C::sub_502DC0()
+{
+    return 0;
+}
+
+STUB_FUNC(0x502f60)
+Miss2_25C* Miss2_25C::sub_502F60()
+{
+    return 0;
+}
+
+STUB_FUNC(0x502f80)
+void Miss2_25C::sub_502F80(s32 a2)
+{
+}
+
+STUB_FUNC(0x502fb0)
+char_type Miss2_25C::sub_502FB0(s32 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x502ff0)
+void Miss2_25C::sub_502FF0(s32 a2, char_type a3)
+{
+}
+
+STUB_FUNC(0x503050)
+void Miss2_25C::sub_503050(s32 a2)
+{
+}
