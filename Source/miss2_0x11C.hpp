7c7a25467a5ebc9634453ae5c31f9c2732e714fe
Valps <valps.rn@gmail.com>
2025-01-21 23:19:21 -0300

    Add new structs for miss2_0x11C

--- Source/miss2_0x11C.hpp
+++ Source/miss2_0x11C.hpp
@@ -25,6 +25,18 @@ struct SCR_XYZ_f
     Fix16 field_8_z;
 };
 
+struct SCR_XY_f
+{
+    Fix16 field_0_x;
+    Fix16 field_4_y;
+};
+
+struct SCR_Rect_f
+{
+    SCR_XYZ_f field_0_pos;
+    SCR_XY_f field_C_size;
+};
+
 struct SCR_XY_uc
 {
     u8 field_0_x;
@@ -36,6 +48,13 @@ struct SCR_XYZ_uc
     u8 field_0_x;
     u8 field_1_y;
     u8 field_2_z;
+};
+
+struct SCR_ONEVAR_RECT : SCR_CMD_HEADER
+{
+    u16 field_8_varname;
+    u16 field_A_unk;
+    SCR_Rect_f field_C_rect;
 };
 
 struct SCR_PLAYER_PED
