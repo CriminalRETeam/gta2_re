cc571b0629b1c29ac950c2d1e1bbe569c4358f4d
Valps <valps.rn@gmail.com>
2025-01-25 18:47:46 -0300

    Simplify if statement in 0x5038D0

--- Source/miss2_0x11C.cpp
+++ Source/miss2_0x11C.cpp
@@ -199,8 +199,7 @@ void miss2_0x11C::SCRCMD_OBJ_DECSET_5038D0(SCR_OBJ_DATA* pCmd, SCR_POINTER* a2)
         }
         else
         {
-            bool is_busy = a2->field_8_obj->check_is_busy_shop();
-            if (is_busy)
+            if (a2->field_8_obj->check_is_busy_shop())
             {
                 a2->field_8_obj->field_26 = pCmd->field_1C_value_shop_type;
             }
