728ecd2549c0ddfe2db7a839c60ac01d241221d8
Paul <github@paulsapps.com>
2025-02-20 02:15:25 +0000

    match Shooey_CC::sub_485140

--- Source/Shooey_CC.hpp
+++ Source/Shooey_CC.hpp
@@ -6,6 +6,7 @@
 class Ped;
 class Player;
 class Car_BC;
+class Char_B4;
 
 class Shooey_14
 {
@@ -32,7 +33,7 @@ class Shooey_CC
     EXPORT void dtor_484FD0();
     EXPORT void sub_484FE0(u32 a2, Ped* pPed);
     EXPORT bool sub_485090(Car_BC* a2, Player* a3);
-    EXPORT char_type sub_4850F0(Shooey_CC* a1, s32 a2, Player* a3);
+    EXPORT char_type sub_4850F0(Char_B4* a2, Player* a3);
     EXPORT char_type sub_485140(Ped* a2, Player* a3);
 
     u16 field_0;
