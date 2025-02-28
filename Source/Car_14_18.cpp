cdc219261c9fcaaf6846045036d09aa989d4ae9f
Valps <valps.rn@gmail.com>
2025-02-28 00:54:25 -0300

    match Car_14_18::sub_59DDF0

--- Source/Car_14_18.cpp
+++ Source/Car_14_18.cpp
@@ -1,6 +1,7 @@
 #include "Car_14_18.hpp"
 #include "map_0x370.hpp"
 #include "PurpleDoom.hpp"
+#include "sprite.hpp"
 
 MATCH_FUNC(0x59dd60)
 void Car_14_18::sub_59DD60()
@@ -11,10 +12,20 @@ void Car_14_18::sub_59DD60()
     dword_6F5F38 = field_C.ToInt();
 }
 
-STUB_FUNC(0x59ddf0)
+MATCH_FUNC(0x59ddf0)
 u32 Car_14_18::sub_59DDF0(Sprite* a2)
 {
-    return 0;
+    Car_8* v3 = (Car_8*)&a2->sprite_4c_ptr->field_C;
+
+    if (inline_sub_4B9FD0(&v3[0]) || inline_sub_4B9FD0(&v3[1]) 
+        || inline_sub_4B9FD0(&v3[2]) || inline_sub_4B9FD0(&v3[3]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 STUB_FUNC(0x59de80)
