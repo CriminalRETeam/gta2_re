720de3fe6b855fd8790d2730632f53355ed3efef
Valps <valps.rn@gmail.com>
2025-02-28 00:05:14 -0300

    Stub Car_14_18

---
+++ Source/Car_14_18.cpp
@@ -0,0 +1,35 @@
+#include "Car_14_18.hpp"
+
+STUB_FUNC(0x59dd60)
+void Car_14_18::sub_59DD60()
+{
+}
+
+STUB_FUNC(0x59ddf0)
+u32 Car_14_18::sub_59DDF0(Sprite* a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x59de80)
+char_type Car_14_18::sub_59DE80()
+{
+    return 0;
+}
+
+STUB_FUNC(0x59dee0)
+void Car_14_18::sub_59DEE0(Fix16 a2, Fix16 a3)
+{
+}
+
+STUB_FUNC(0x59df20)
+bool Car_14_18::sub_59DF20(u8 a2)
+{
+    return 0;
+}
+
+STUB_FUNC(0x59dfb0)
+bool Car_14_18::sub_59DFB0(s32 a2)
+{
+    return 0;
+}
---
+++ Source/Car_14_18.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Car_BC.hpp"
+#include "Function.hpp"
+#include "fix16.hpp"
+class Sprite;
+
+class Car_14_18
+{
+  public:
+    EXPORT void sub_59DD60();
+    EXPORT u32 sub_59DDF0(Sprite* a2);
+    EXPORT char_type sub_59DE80();
+    EXPORT void sub_59DEE0(Fix16 a2, Fix16 a3);
+    EXPORT bool sub_59DF20(u8 a2);
+    EXPORT bool sub_59DFB0(s32 a2);
+
+    inline bool inline_sub_4B9FD0(Car_8* a2)
+    {
+        return Fix16::inline_is_between(field_0, field_4, a2->field_0) && Fix16::inline_is_between(field_8, field_C, a2->field_4);
+    }
+
+    Fix16 field_0;
+    Fix16 field_4;
+    Fix16 field_8;
+    Fix16 field_C;
+    Fix16 field_10;
+    Fix16 field_14;
+};
