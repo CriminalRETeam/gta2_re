c5e2821330c64aedd881c01ef0e2d385776c3d94
MrSapps <github@paulsapps.com>
2025-05-03 16:26:05 +0100

    match Object_8::Object_8

--- Source/Object_8.cpp
+++ Source/Object_8.cpp
@@ -1,8 +1,12 @@
 #include "Object_8.hpp"
 
-STUB_FUNC(0x52ad50)
+MATCH_FUNC(0x52ad50)
 Object_8::Object_8()
 {
+    this->field_0 = 0;
+    this->field_7 = 99;
+    this->field_6 = 99;
+    this->field_4 = 99;
 }
 
 MATCH_FUNC(0x52ad70)
--- Source/Object_8.hpp
+++ Source/Object_8.hpp
@@ -8,5 +8,7 @@ class Object_8
     EXPORT Object_8();
     EXPORT ~Object_8();
     Object_8* field_0;
-    s32 field_4;
+    s16 field_4;
+    char_type field_6;
+    char_type field_7;
 };
