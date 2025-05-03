df17050b756f9d8cd007639cd20ce6588c4b3ea4
MrSapps <github@paulsapps.com>
2025-05-03 16:36:57 +0100

    match Object_29178::~Object_29178

--- Source/Object_29178.cpp
+++ Source/Object_29178.cpp
@@ -1,10 +1,12 @@
 #include "Object_29178.hpp"
 #include "Globals.hpp"
 
-EXPORT_VAR Object_29178 *gObject_29178_6F8F80;
+EXPORT_VAR Object_29178* gObject_29178_6F8F80;
 GLOBAL(gObject_29178_6F8F80, 0x6F8F80);
 
-STUB_FUNC(0x52a720)
+MATCH_FUNC(0x52a720)
 Object_29178::~Object_29178()
 {
+    this->field_0 = 0;
+    this->field_4 = 0;
 }
