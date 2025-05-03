94699b2caea88788924a5e0b55e1ef554daf3fb6
MrSapps <github@paulsapps.com>
2025-05-03 16:36:24 +0100

    match Object_5A40::~Object_5A40

--- Source/Object_5A40.cpp
+++ Source/Object_5A40.cpp
@@ -4,9 +4,9 @@
 EXPORT_VAR Object_5A40* gObject_5A40_6F8F7C;
 GLOBAL(gObject_5A40_6F8F7C, 0x6F8F7C);
 
-STUB_FUNC(0x52a770)
+MATCH_FUNC(0x52a770)
 Object_5A40::~Object_5A40()
 {
-
+    field_0 = 0;
 }
 
