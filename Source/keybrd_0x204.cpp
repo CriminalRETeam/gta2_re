a559015910bd49d97027413fa563801ea2758bff
MrSapps <github@paulsapps.com>
2025-05-23 16:47:28 +0100

    match keybrd_0x204::create_4D5F50 by using correct function order to stop SEH generation

--- Source/keybrd_0x204.cpp
+++ Source/keybrd_0x204.cpp
@@ -8,31 +8,7 @@
 EXPORT_VAR keybrd_0x204* gKeybrd_0x204_6F52F4;
 GLOBAL(gKeybrd_0x204_6F52F4, 0x6F52F4);
 
-STUB_FUNC(0x4D5F50)
-void __stdcall keybrd_0x204::create_4D5F50()
-{
-    NOT_IMPLEMENTED;
-    // no match, OG hasn't got SEH here ??
-    if (!gKeybrd_0x204_6F52F4)
-    {
-        gKeybrd_0x204_6F52F4 = new keybrd_0x204();
-        if (!gKeybrd_0x204_6F52F4)
-        {
-            FatalError_4A38C0(32, "C:\\Splitting\\Gta2\\Source\\keybrd.cpp", 244);
-        }
-    }
-    gKeybrd_0x204_6F52F4->LoadKbCfg_4D5E00();
-}
-
-MATCH_FUNC(0x4D5FA0)
-void keybrd_0x204::destroy_4D5FA0()
-{
-    if (gKeybrd_0x204_6F52F4)
-    {
-        delete gKeybrd_0x204_6F52F4;
-        gKeybrd_0x204_6F52F4 = 0;
-    }
-}
+char dword_620D2C[] = {' ', ' ', 0};
 
 MATCH_FUNC(0x4D5D70)
 keybrd_0x204::keybrd_0x204()
@@ -45,6 +21,29 @@ MATCH_FUNC(0x4D5D90)
 keybrd_0x204::~keybrd_0x204()
 {
 }
+
+
+static inline bool is_line_break_or_space(wchar_t letter)
+{
+    return letter == '\n' || letter == ' ';
+}
+
+MATCH_FUNC(0x4D5DA0)
+void keybrd_0x204::ReadCfg_4D5DA0(FILE* Stream, wchar_t* pOut)
+{
+    u8 i = 0;
+    for (;;)
+    {
+        const wchar_t read_char = fgetc(Stream);
+        if (is_line_break_or_space(read_char))
+        {
+            pOut[i] = 0;
+            return;
+        }
+        pOut[i++] = read_char;
+    }
+}
+
 
 MATCH_FUNC(0x4D5E00)
 void keybrd_0x204::LoadKbCfg_4D5E00()
@@ -118,7 +117,56 @@ void keybrd_0x204::LoadKbCfg_4D5E00()
     fclose(hConfigFile);
 }
 
-char dword_620D2C[] = {' ', ' ', 0};
+MATCH_FUNC(0x4D5F40)
+wchar_t keybrd_0x204::GetKey_4D5F40(u16 key_idx)
+{
+    return field_0_keys[key_idx];
+}
+
+
+MATCH_FUNC(0x4D5F50)
+void __stdcall keybrd_0x204::create_4D5F50()
+{
+    if (!gKeybrd_0x204_6F52F4)
+    {
+        gKeybrd_0x204_6F52F4 = new keybrd_0x204();
+        if (!gKeybrd_0x204_6F52F4)
+        {
+            FatalError_4A38C0(32, "C:\\Splitting\\Gta2\\Source\\keybrd.cpp", 244);
+        }
+    }
+    gKeybrd_0x204_6F52F4->LoadKbCfg_4D5E00();
+}
+
+MATCH_FUNC(0x4D5FA0)
+void keybrd_0x204::destroy_4D5FA0()
+{
+    if (gKeybrd_0x204_6F52F4)
+    {
+        delete gKeybrd_0x204_6F52F4;
+        gKeybrd_0x204_6F52F4 = 0;
+    }
+}
+
+
+MATCH_FUNC(0x4D5FD0)
+void keybrd_0x204::RecreateIfLayoutChanged_4D5FD0()
+{
+    if (gKeybrd_0x204_6F52F4)
+    {
+        s32 curLayout = gKeybrd_0x204_6F52F4->field_200_keyBoardLayout;
+        if (curLayout == keybrd_0x204::GetLayout_4D6000())
+        {
+            return;
+        }
+        keybrd_0x204::destroy_4D5FA0();
+        keybrd_0x204::create_4D5F50();
+    }
+    else
+    {
+        keybrd_0x204::create_4D5F50();
+    }
+}
 
 STUB_FUNC(0x4D6000)
 s32 keybrd_0x204::GetLayout_4D6000()
@@ -166,50 +214,4 @@ s32 keybrd_0x204::GetLayout_4D6000()
             break;
     }
     return result;
-}
-
-static inline bool is_line_break_or_space(wchar_t letter)
-{
-    return letter == '\n' || letter == ' ';
-}
-
-MATCH_FUNC(0x4D5DA0)
-void keybrd_0x204::ReadCfg_4D5DA0(FILE* Stream, wchar_t* pOut)
-{
-    u8 i = 0;
-    for (;;)
-    {
-        const wchar_t read_char = fgetc(Stream);
-        if (is_line_break_or_space(read_char))
-        {
-            pOut[i] = 0;
-            return;
-        }
-        pOut[i++] = read_char;
-    }
-}
-
-MATCH_FUNC(0x4D5F40)
-wchar_t keybrd_0x204::GetKey_4D5F40(u16 key_idx)
-{
-    return field_0_keys[key_idx];
-}
-
-MATCH_FUNC(0x4D5FD0)
-void keybrd_0x204::RecreateIfLayoutChanged_4D5FD0()
-{
-    if (gKeybrd_0x204_6F52F4)
-    {
-        s32 curLayout = gKeybrd_0x204_6F52F4->field_200_keyBoardLayout;
-        if (curLayout == keybrd_0x204::GetLayout_4D6000())
-        {
-            return;
-        }
-        keybrd_0x204::destroy_4D5FA0();
-        keybrd_0x204::create_4D5F50();
-    }
-    else
-    {
-        keybrd_0x204::create_4D5F50();
-    }
 }
--- Source/keybrd_0x204.hpp
+++ Source/keybrd_0x204.hpp
@@ -6,28 +6,26 @@
 class keybrd_0x204
 {
   public:
-    s16 field_0_keys[256];
-    s32 field_200_keyBoardLayout;
+    EXPORT keybrd_0x204();
 
-    // todo: func order
+    EXPORT ~keybrd_0x204();
+
+    EXPORT void ReadCfg_4D5DA0(FILE* Stream, wchar_t* pOut);
+
+    EXPORT void LoadKbCfg_4D5E00();
+
+    EXPORT wchar_t GetKey_4D5F40(u16 a1);
 
     EXPORT static void __stdcall create_4D5F50();
 
     EXPORT static void destroy_4D5FA0();
 
-    EXPORT keybrd_0x204();
-
-    EXPORT ~keybrd_0x204();
-
-    EXPORT void LoadKbCfg_4D5E00();
+    EXPORT static void RecreateIfLayoutChanged_4D5FD0();
 
     EXPORT static s32 GetLayout_4D6000();
 
-    EXPORT void ReadCfg_4D5DA0(FILE* Stream, wchar_t* pOut);
-
-    EXPORT wchar_t GetKey_4D5F40(u16 a1);
-
-    EXPORT static void RecreateIfLayoutChanged_4D5FD0();
+    s16 field_0_keys[256];
+    s32 field_200_keyBoardLayout;
 };
 
 EXPORT_VAR extern keybrd_0x204* gKeybrd_0x204_6F52F4;
