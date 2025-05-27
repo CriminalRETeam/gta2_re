bd257f7e69168868a62f5b69da1523f915765af9
Valps <valps.rn@gmail.com>
2025-05-27 17:36:28 -0300

    Cleaning up Map_0x370::sub_4E6400

--- Source/map_0x370.cpp
+++ Source/map_0x370.cpp
@@ -1274,43 +1274,40 @@ gmp_block_info* Map_0x370::sub_4E6360(s32 a2, s32 a3, s32* a4)
 }
 
 MATCH_FUNC(0x4E6400)
-Fix16* Map_0x370::sub_4E6400(Fix16* a2, Fix16 a3, Fix16 a4, Fix16 a5)
+Fix16* Map_0x370::sub_4E6400(Fix16* a2, Fix16 x_pos, Fix16 y_pos, Fix16 z_pos)
 {
-    Fix16 v5;
-    Fix16 v6;
-    Fix16 v7;
     gmp_block_info* block_4DFE10;
-    u8 v9;
-    Fix16 v10;
-    u8 v13;
+    u8 slope_byte;
+    Fix16 new_z;
 
-    v5 = a5;
-    v6 = a4;
-    v7 = a3;
-    if ((a5.mValue & 0x3FFF) == dword_6F610C.mValue || (block_4DFE10 = Map_0x370::get_block_4DFE10(a3.ToInt(), a4.ToInt(), a5.ToInt())) == 0 ||
-        (v9 = block_4DFE10->field_B_slope_type, (v13 = v9 & 0xFC) <= 0) || v13 >= 0xB4 || (v9 & 3) == 0 ||
-        (v10.mValue = v5.mValue & 0xFFFFC000, Map_0x370::sub_4E5BF0(v7, v6, &v10), v10 > v5))
+    if (z_pos.GetFracValue() == dword_6F610C 
+        || (block_4DFE10 = get_block_4DFE10(x_pos.ToInt(), y_pos.ToInt(), z_pos.ToInt())) == NULL 
+        || (slope_byte = block_4DFE10->field_B_slope_type, 
+            !is_climbing_slope(slope_byte)) 
+        || is_air_type(slope_byte) 
+        || (new_z = z_pos.GetRoundValue(), 
+            Map_0x370::sub_4E5BF0(x_pos, y_pos, &new_z), new_z > z_pos))
     {
-        s32 v14 = v5.ToInt() - 1;
-        gmp_block_info* v11 = Map_0x370::sub_4E6360(v7.ToInt(), v6.ToInt(), &v14);
-        gBlockInfo0_6F5EB0 = v11;
-        if (!v11)
+        s32 v14 = z_pos.ToInt() - 1;
+        gmp_block_info* pBlock = Map_0x370::sub_4E6360(x_pos.ToInt(), y_pos.ToInt(), &v14);
+        gBlockInfo0_6F5EB0 = pBlock;
+        if (pBlock == NULL)
         {
-            a2->mValue = 0x4000;
+            *a2 = Fix16(1);
             return a2;
         }
-        s8 field_B_slope_type = v11->field_B_slope_type;
-        if ((v13 = field_B_slope_type & 0xFCu) > 0 && v13 < 0xB4u && (field_B_slope_type & 3) != 0)
+        slope_byte = pBlock->field_B_slope_type;
+        if (is_climbing_slope(slope_byte) && !is_air_type(slope_byte))
         {
-            v10.FromInt(v14);
-            Map_0x370::sub_4E5BF0(v7, v6, &v10);
+            new_z = Fix16(v14);
+            Map_0x370::sub_4E5BF0(x_pos, y_pos, &new_z);
         }
         else
         {
-            v10.FromInt(v14 + 1);
+            new_z = Fix16(v14) + 1;
         }
     }
-    a2->mValue = v10.mValue;
+    *a2 = new_z;
     return a2;
 }
 
