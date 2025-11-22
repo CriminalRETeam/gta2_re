37c7f4a65d0ce25a6d1c9d74bcb128460421f967
MrSapps <github@paulsapps.com>
2025-11-22 03:33:53 +0000

    use pool obj in Object_8_Pool

--- Source/Object_8_Pool.cpp
+++ Source/Object_8_Pool.cpp
@@ -6,5 +6,5 @@ DEFINE_GLOBAL(Object_8_Pool*, gObject_8_Pool_6F8F78, 0x6F8F78);
 MATCH_FUNC(0x52a750)
 Object_8_Pool::~Object_8_Pool()
 {
-    field_0 = 0;
+
 }
--- Source/Object_8_Pool.hpp
+++ Source/Object_8_Pool.hpp
@@ -1,22 +1,18 @@
 #pragma once
 
 #include "Object_8.hpp"
+#include "Pool.hpp"
 
 class Object_8_Pool
 {
   public:
     Object_8_Pool()
     {
-        for (s32 j = 0; j < 1100; j++)
-        {
-            field_4[j].mpNext = &field_4[j+1];
-        }
-        field_4[1100].mpNext = 0;
-        field_0 = &field_4[0];
+
     }
     EXPORT ~Object_8_Pool();
-    Object_8* field_0;
-    Object_8 field_4[1101];
+
+    PoolBasic<Object_8, 1101> field_0_pool;
 };
 
 
