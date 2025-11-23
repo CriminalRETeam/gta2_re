c2c4be7297a1ebae181e0de6ae5968ebef902645
MrSapps <github@paulsapps.com>
2025-11-23 06:11:46 +0000

    match Char_C::sub_470CC0

--- Source/char.cpp
+++ Source/char.cpp
@@ -563,11 +563,24 @@ Ped* Char_C::SpawnGangDriver_470BA0(Car_BC* pCar, Gang_144* pGang)
     return 0;
 }
 
-STUB_FUNC(0x470cc0)
-Ped* Char_C::sub_470CC0(Car_BC* a2)
+MATCH_FUNC(0x470cc0)
+Ped* Char_C::sub_470CC0(Car_BC* pCar)
 {
-    NOT_IMPLEMENTED;
-    return 0;
+    Ped* pNewPed = gPedPool_6787B8->Allocate();
+    pNewPed->field_244_remap = -1;
+    pNewPed->field_26C_graphic_type = 0;
+    pNewPed->field_238 = 3;
+    pNewPed->field_240_occupation = 4;//unknown_2;
+    pNewPed->field_16C_car = pCar;
+    pNewPed->field_168_game_object = 0;
+    pNewPed->sub_45C500(10);
+    pNewPed->sub_45C540(10);
+    pNewPed->field_24C_target_car_door = 0;
+    pNewPed->field_288_threat_search = 2;//area_2;
+    pNewPed->field_28C_threat_reaction = 3;//run_away_3;
+    pNewPed->field_216_health = 100;
+    pNewPed->field_26C_graphic_type = 0;
+    return pNewPed;
 }
 
 STUB_FUNC(0x470d60)
