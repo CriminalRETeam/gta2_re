7bc7c67c67cde8117ed589384da61d888c15781d
MrSapps <github@paulsapps.com>
2025-11-23 01:41:30 +0000

    crusher renames

--- Source/Crushers.cpp
+++ Source/Crushers.cpp
@@ -1,6 +1,6 @@
 #include "Crushers.hpp"
-#include "Car_BC.hpp"
 #include "Camera.hpp"
+#include "Car_BC.hpp"
 #include "Globals.hpp"
 #include "infallible_turing.hpp"
 #include "root_sound.hpp"
@@ -11,26 +11,26 @@ DEFINE_GLOBAL(CrusherPool_94*, gCrusherPool_94_67A830, 0x67A830);
 MATCH_FUNC(0x4882d0)
 Crusher_30::Crusher_30()
 {
-    field_18 = NULL;
+    field_18_sound = NULL;
 }
 
 MATCH_FUNC(0x4882e0)
 Crusher_30::~Crusher_30()
 {
-    if (field_18)
+    if (field_18_sound)
     {
-        gRoot_sound_66B038.DestroySoundObj_40FE60(field_18);
-        field_18 = NULL;
+        gRoot_sound_66B038.DestroySoundObj_40FE60(field_18_sound);
+        field_18_sound = NULL;
     }
 }
 
 MATCH_FUNC(0x488310)
-void Crusher_30::sub_488310(Car_BC* pCar)
+void Crusher_30::CrushCar_488310(Car_BC* pCar)
 {
-    if (!field_2C && pCar->field_88 != 5 && !pCar->sub_43DC00() && pCar->field_9C != 7)
+    if (field_2C_state == 0 && pCar->field_88 != 5 && !pCar->sub_43DC00() && pCar->field_9C != 7)
     {
         field_14_pObj = pCar;
-        field_2C = 1;
+        field_2C_state = 1;
         pCar->field_78_flags |= 2u;
     }
 }
@@ -42,18 +42,17 @@ void Crusher_30::Service_488350()
 }
 
 STUB_FUNC(0x4885a0)
-infallible_turing* Crusher_30::sub_4885A0(Fix16 a2, Fix16 a3, char_type a4)
+void Crusher_30::InitCrusher_4885A0(Fix16 xpos, Fix16 ypos, char_type crusher_idx)
 {
     NOT_IMPLEMENTED;
-    return 0;
 }
 
 MATCH_FUNC(0x4887a0)
-bool CrusherPool_94::sub_4887A0(Car_BC* toFind)
+bool CrusherPool_94::IsCarBeingCrushed_4887A0(Car_BC* pCar)
 {
-    for (s32 i=0; i < field_90_count; i++)
+    for (s32 i = 0; i < field_90_count; i++)
     {
-        if (field_0[i].field_14_pObj == toFind)
+        if (field_0[i].field_14_pObj == pCar)
         {
             return true;
         }
@@ -62,9 +61,9 @@ bool CrusherPool_94::sub_4887A0(Car_BC* toFind)
 }
 
 MATCH_FUNC(0x4887d0)
-void CrusherPool_94::sub_4887D0(Car_BC* a2, u8 idx)
+void CrusherPool_94::CrushCarWithCrusher_4887D0(Car_BC* pCar, u8 crusher_idx)
 {
-    field_0[idx].sub_488310(a2);
+    field_0[crusher_idx].CrushCar_488310(pCar);
 }
 
 MATCH_FUNC(0x4887f0)
@@ -81,10 +80,10 @@ void CrusherPool_94::CrushersService_4887F0()
 }
 
 MATCH_FUNC(0x488820)
-Crusher_30* CrusherPool_94::CreateCrusher_488820(Fix16 a2, Fix16 a3)
+Crusher_30* CrusherPool_94::CreateCrusher_488820(Fix16 xpos, Fix16 ypos)
 {
     Crusher_30* pNew = &field_0[field_90_count];
-    pNew->sub_4885A0(a2, a3, field_90_count);
+    pNew->InitCrusher_4885A0(xpos, ypos, field_90_count);
     field_90_count++;
     return pNew;
 }
--- Source/Crushers.hpp
+++ Source/Crushers.hpp
@@ -5,36 +5,46 @@
 
 class Car_BC;
 class infallible_turing;
+class Object_2C;
 
 class Crusher_30
 {
   public:
     EXPORT Crusher_30(); // 4882D0
     EXPORT ~Crusher_30(); // 4882E0
-    EXPORT void sub_488310(Car_BC* a2);
+    EXPORT void CrushCar_488310(Car_BC* pCar);
     EXPORT void Service_488350();
-    EXPORT infallible_turing* sub_4885A0(Fix16 a2, Fix16 a3, char_type a4);
-    s32 field_0;
-    s32 field_4;
-    s32 field_8;
-    s32 field_C;
+    EXPORT void InitCrusher_4885A0(Fix16 xpos, Fix16 ypos, char_type crusher_idx);
+
+    // Each side of the crusher?
+    Object_2C* field_0;
+    Object_2C* field_4;
+    Object_2C* field_8;
+    Object_2C* field_C;
+
     s32 field_10;
+
+    // Car getting crunched?
     Car_BC* field_14_pObj;
-    infallible_turing* field_18;
+
+    infallible_turing* field_18_sound;
+
     s32 field_1C;
     s32 field_20;
-    s32 field_24;
-    s32 field_28;
-    s32 field_2C;
+
+    s32 field_24_xpos;
+    s32 field_28_ypos;
+
+    s32 field_2C_state;
 };
 
 class CrusherPool_94
 {
   public:
-    EXPORT bool sub_4887A0(Car_BC* toFind);
-    EXPORT void sub_4887D0(Car_BC* a2, u8 idx);
+    EXPORT bool IsCarBeingCrushed_4887A0(Car_BC* pCar);
+    EXPORT void CrushCarWithCrusher_4887D0(Car_BC* pCar, u8 crusher_idx);
     EXPORT void CrushersService_4887F0();
-    EXPORT Crusher_30* CreateCrusher_488820(Fix16 a2, Fix16 a3);
+    EXPORT Crusher_30* CreateCrusher_488820(Fix16 xpos, Fix16 ypos);
     EXPORT CrusherPool_94();
     EXPORT ~CrusherPool_94();
 
