55264889890f849751565b5b95855d13cf21ceff
MrSapps <github@paulsapps.com>
2025-11-29 14:08:37 +0000

    use pool func in lighting

--- Source/Light_1D4CC.hpp
+++ Source/Light_1D4CC.hpp
@@ -15,41 +15,7 @@ class LightBase
 
     void sub_45BF50()
     {
-        nostalgic_ellis_0x28* pCurLight = field_0_pool.field_4_pPrev;
-        field_0_pool.field_X_count = 0;
-        
-        nostalgic_ellis_0x28* pNext;
-        while (pCurLight)
-        {
-            ++field_0_pool.field_X_count;
-            
-            pNext = pCurLight->mpNext;
-            pCurLight->field_17_off_time--;
-            if (!pCurLight->field_17_off_time )
-            {
-                if (pCurLight->field_0 & 0xff) 
-                {
-                    pCurLight->field_0 &= ~0xff;
-                    pCurLight->field_17_off_time = pCurLight->field_15_off_time;
-                    if (pCurLight->field_16_shape)
-                    {
-                        pCurLight->field_17_off_time += stru_6F6784.get_uint8_4F7B70(&pCurLight->field_16_shape);
-                    }
-                }
-                else
-                {
-                    pCurLight->field_0 &= ~0xff;
-                    u8 t = pCurLight->field_18_intensity;
-                    pCurLight->field_0 |= t;
-                    pCurLight->field_17_off_time = pCurLight->field_14_on_time;
-                    if (pCurLight->field_16_shape)
-                    {
-                        pCurLight->field_17_off_time += stru_6F6784.get_uint8_4F7B70(&pCurLight->field_16_shape);
-                    }
-                }
-            }
-            pCurLight = pNext;
-        }
+        field_0_pool.UpdatePool();
     }
 
     void sub_45C1E0()
