6090dfaedb37aa066a32a7696604b41772f6a9eb
MrSapps <github@paulsapps.com>
2026-01-30 06:14:42 +0000

    wip PurpleDoom::FindNearestSpriteInRow_478880

--- Source/PurpleDoom.cpp
+++ Source/PurpleDoom.cpp
@@ -747,11 +747,70 @@ bool PurpleDoom::CheckAndHandleRowCollisionsForSprite_4787E0(u32 y_pos, Sprite* 
     return bRet;
 }
 
-STUB_FUNC(0x478880)
-Sprite* PurpleDoom::FindNearestSpriteInRow_478880(u32 a2, Sprite* a3)
+WIP_FUNC(0x478880)
+Sprite* PurpleDoom::FindNearestSpriteInRow_478880(u32 y_pos, Sprite* pSprite)
 {
-    NOT_IMPLEMENTED;
-    return 0;
+    WIP_IMPLEMENTED;
+ 
+    PurpleDoom_C* pXItem = GetFirstXCellInRow_478590(y_pos);
+    if (!pXItem)
+    {
+        return 0;
+    }
+
+    Collide_8* pObj;
+    Sprite* pSprt;
+    while (1)
+    {
+        if (pXItem->field_0_x_len > gPurple_right_6F5B80)
+        {
+            return 0;
+        }
+
+        pObj = pXItem->field_4_p8;
+        if (pObj)
+        {
+            break;
+        }
+    LABEL_14:
+        pXItem = pXItem->mpNext;
+        if (!pXItem)
+        {
+            return 0;
+        }
+    }
+    while (1)
+    {
+        pSprt = pObj->field_0_sprt;
+        if (pObj->field_0_sprt == gPurpleDoom_exclusion_sprite_678F84 ||
+            gPurpleDoom_smallestDistSprite_678E40 && !pSprt->TypeIs_446940(gPurpleDoom_exclude_type_678F60) ||
+            pSprt->field_C_sprite_4c_ptr->CollisionIdIs_446930(gCollide_C_6791FC->field_4_count) || !pSprt->sub_59E850(pSprite))
+        {
+            goto go_next;
+        }
+
+        gCollide_C_6791FC->field_0_count++;
+        if (pSprite->CollisionCheck_59E590(pObj->field_0_sprt))
+        {
+            break;
+        }
+    save_stamp:
+        pObj->field_0_sprt->field_C_sprite_4c_ptr->SetCollisionId_446920(gCollide_C_6791FC->field_4_count);
+    go_next:
+        pObj = pObj->mpNext;
+        if (!pObj)
+        {
+            goto LABEL_14;
+        }
+    }
+
+    if (!pObj->field_0_sprt->TypeIs_446940(gPurpleDoom_exclude_type_678F60))
+    {
+        gPurpleDoom_smallestDistSprite_678E40 = pObj->field_0_sprt;
+        goto save_stamp;
+    }
+
+    return pObj->field_0_sprt;
 }
 
 MATCH_FUNC(0x478950)
