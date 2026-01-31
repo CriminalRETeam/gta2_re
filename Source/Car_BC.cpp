b8ba4cba3bc0b708f38f52a1179e0e2f4458994a
MrSapps <github@paulsapps.com>
2026-01-31 14:01:07 +0000

    wip Car_BC::sub_4406E0

--- Source/Car_BC.cpp
+++ Source/Car_BC.cpp
@@ -3032,10 +3032,66 @@ void Car_BC::ShowCarName_4406B0(Ped* pPed)
     }
 }
 
-STUB_FUNC(0x4406e0)
-void Car_BC::sub_4406E0(Ped* a2)
+WIP_FUNC(0x4406e0)
+void Car_BC::sub_4406E0(Ped* pPed)
 {
-    NOT_IMPLEMENTED;
+    WIP_IMPLEMENTED;
+
+    Player* pPlayer; // ebp
+    s32 f_88; // eax
+    wchar_t* pCarName; // eax
+    CarPhysics_B0* pPhysics; // eax
+
+    pPlayer = pPed->field_15C_player;
+    SetDriver(pPed);
+    InitCarAIControl_440590();
+    this->field_7C_uni_num = pPed->field_238;
+    this->field_76 = 0;
+    if (pPed->IsField238_45EDE0(2))
+    {
+        sub_4446E0();
+    }
+    
+    f_88 = this->field_88;
+    if (f_88 == 2 || f_88 == 4 || f_88 == 3)
+    {
+        this->field_88 = 1;
+    }
+
+    if ((this->field_8D & 1) != 0)
+    {
+        if (gfrosty_pasteur_6F8060->sub_512910(pPed->field_200_id, this->field_6C_maybe_id))
+        {
+            this->field_8D &= ~1u;
+        }
+    }
+    if (pPlayer)
+    {
+        if (pPlayer->field_0_bIsUser)
+        {
+            if (this->field_54_driver->field_240_occupation != ped_ocupation_enum::empty)
+            {
+                pCarName = GetCarStr_439F80();
+                gHud_2B00_706620->sub_5D5240(pCarName);
+            }
+        }
+        sub_443E50();
+        pPlayer->sub_5645B0(this);
+        if (this->field_54_driver->field_240_occupation != ped_ocupation_enum::empty)
+        {
+            pPlayer->sub_564AD0(this);
+        }
+        pPhysics = this->field_58_physics;
+        if (pPhysics)
+        {
+            pPhysics->field_8C_state = 2;
+        }
+        this->field_95 = pPed->field_200_id;
+    }
+    else
+    {
+        this->field_95 = 0; // TODO: field_95_ped_id rename
+    }
 }
 
 MATCH_FUNC(0x4407f0)
