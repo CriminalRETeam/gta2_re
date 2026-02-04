f4b222d210de5da72460b237519eedcd9e642acd
MrSapps <github@paulsapps.com>
2026-02-04 12:54:40 +0000

    match CarPhysics_B0::sub_55F930

--- Source/CarPhysics_B0.hpp
+++ Source/CarPhysics_B0.hpp
@@ -84,7 +84,7 @@ class CarPhysics_B0
     EXPORT void sub_55F740(Fix16_Point* a2, Fix16_Point* a3);
     EXPORT void sub_55F7A0(Fix16_Point* a2, Fix16_Point a3);
     EXPORT s32 sub_55F800(Fix16_Point* a2, Fix16_Point* a3, s32 a4);
-    EXPORT s32 sub_55F930(Fix16_Point* a2);
+    EXPORT void sub_55F930(Fix16_Point* a2);
     EXPORT void sub_55F970(Fix16 a2);
     EXPORT void ApplyForceScaledByMass_55F9A0(Fix16_Point_POD& pForce);
     EXPORT void sub_55FA10(Fix16_Point* a2);
