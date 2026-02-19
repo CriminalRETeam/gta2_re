9a6a765421c3a5c0dd6de8b3816759a437702eb2
disinvite <disinvite@users.noreply.github.com>
2026-02-18 23:04:34 -0500

    Remove Fix16::FromInt functions. Mark Fix16 ctors for reccmp

--- Source/fix16.hpp
+++ Source/fix16.hpp
@@ -281,10 +281,6 @@ class Fix16
         return Fix16(sqrt(input.AsDouble()));
     }
 
-    EXPORT Fix16& FromInt_4369F0(s32 a2);
-    EXPORT Fix16& FromInt_45C4E0(u8 a2);
-    EXPORT Fix16& FromU16_4AE970(u16 a2);
-
     EXPORT static Fix16 __stdcall Max_44E540(Fix16& pLhs, Fix16& pRhs);
     EXPORT inline static Fix16 __stdcall Abs_436A50(Fix16& a2);
     EXPORT inline static Fix16 __stdcall SquareRoot_436A70(Fix16& a2);
