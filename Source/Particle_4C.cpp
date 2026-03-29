660524ee34cabfcd61c598c66a7e0238a1a4e451
MrSapps <github@paulsapps.com>
2026-03-29 02:46:05 +0100

    match Particle_4C::PoolDeallocate_53E2E0

--- Source/Particle_4C.cpp
+++ Source/Particle_4C.cpp
@@ -1,150 +1,137 @@
 #include "Particle_4C.hpp"
+#include "sprite.hpp"
 
 STUB_FUNC(0x538060)
-char_type Particle_4C::sub_538060()
+char_type Particle_4C::UpdateFloatingParticle_state_6_15_16_17_538060()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x5384c0)
-char_type Particle_4C::sub_5384C0()
+char_type Particle_4C::UpdateDirectedProjectile_state_3_12_5384C0()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x538a40)
-char_type Particle_4C::sub_538A40()
+char_type Particle_4C::UpdateBeamSegment_state_43_538A40()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x538ac0)
-char_type Particle_4C::sub_538AC0()
+char_type Particle_4C::UpdateObjectBeamLink_state_38_538AC0()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x539040)
-char_type Particle_4C::sub_539040()
+char_type Particle_4C::UpdateDirectedBurstSweep_state_4_539040()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x539480)
-char_type Particle_4C::sub_539480()
+char_type Particle_4C::UpdateDirectedBurst_state_13_14_36_539480()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x539890)
-char_type Particle_4C::sub_539890()
+char_type Particle_4C::UpdateCircularBurst_state_5_539890()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53a180)
-char_type Particle_4C::sub_53A180()
+char_type Particle_4C::UpdateStaticAnim_state_39_53A180()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53a280)
-char_type Particle_4C::sub_53A280()
+char_type Particle_4C::UpdateSkidOrScrapeSpark_state_40_41_53A280()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53ab70)
-char_type Particle_4C::sub_53AB70()
+char_type Particle_4C::Empty_state_42_53AB70()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53aba0)
-char_type Particle_4C::sub_53ABA0()
+char_type Particle_4C::UpdateSimpleBallisticMotion_state_1_53ABA0()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53ae60)
-char_type Particle_4C::sub_53AE60()
+char_type Particle_4C::UpdateLargeBallisticDebris_state_35_53AE60()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53b170)
-char_type Particle_4C::sub_53B170()
+char_type Particle_4C::Empty_state_44_53B170()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53b1a0)
-char_type Particle_4C::sub_53B1A0()
+void Particle_4C::UpdateDebrisArc_state_7_53B1A0()
 {
-    NOT_IMPLEMENTED;
-    return 0;
 }
 
 STUB_FUNC(0x53b580)
-char_type Particle_4C::sub_53B580()
+char_type Particle_4C::UpdateShortAnim_state_37_53B580()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53b670)
-char_type Particle_4C::sub_53B670()
+char_type Particle_4C::UpdateAttachedEmitter_state_9_10_53B670()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53b9f0)
-char_type Particle_4C::sub_53B9F0()
+char_type Particle_4C::UpdateBurstAnimation_state_29_30_53B9F0()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53bac0)
-char_type Particle_4C::sub_53BAC0()
+char_type Particle_4C::UpdateCollisionBurst_state_31_34_53BAC0()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 STUB_FUNC(0x53d260)
-char_type Particle_4C::Service_53D260()
+s32 Particle_4C::Service_53D260()
 {
-    NOT_IMPLEMENTED;
     return 0;
 }
 
 MATCH_FUNC(0x53e2c0)
-void Particle_4C::sub_53E2C0()
+void Particle_4C::PoolAllocate_53E2C0()
 {
-    field_8 = 0;
-    field_C = 0;
+    field_8_xpos = 0;
+    field_C_ypos = 0;
     field_10 = 0;
-    field_40 = 0;
-    field_4 &= ~1;
+    field_40_pUnknown = 0;
+    field_4_flags &= ~1;
 }
 
-STUB_FUNC(0x53e2e0)
-void Particle_4C::sub_53E2E0()
+MATCH_FUNC(0x53e2e0)
+void Particle_4C::PoolDeallocate_53E2E0()
 {
-    NOT_IMPLEMENTED;
+    if (field_30_pNext)
+    {
+        field_30_pNext->field_2C &= ~4u;
+        field_30_pNext->field_2C = 0;
+        gSprite_Pool_703818->remove(field_30_pNext);
+        field_30_pNext = 0;
+    }
 }
--- Source/Particle_4C.hpp
+++ Source/Particle_4C.hpp
@@ -4,36 +4,37 @@
 #include "ang16.hpp"
 
 class Sprite;
+class Wolfy_30;
 
 class Particle_4C
 {
   public:
-    EXPORT char_type sub_538060();
-    EXPORT char_type sub_5384C0();
-    EXPORT char_type sub_538A40();
-    EXPORT char_type sub_538AC0();
-    EXPORT char_type sub_539040();
-    EXPORT char_type sub_539480();
-    EXPORT char_type sub_539890();
-    EXPORT char_type sub_53A180();
-    EXPORT char_type sub_53A280();
-    EXPORT char_type sub_53AB70();
-    EXPORT char_type sub_53ABA0();
-    EXPORT char_type sub_53AE60();
-    EXPORT char_type sub_53B170();
-    EXPORT char_type sub_53B1A0();
-    EXPORT char_type sub_53B580();
-    EXPORT char_type sub_53B670();
-    EXPORT char_type sub_53B9F0();
-    EXPORT char_type sub_53BAC0();
-    EXPORT char_type Service_53D260();
-    EXPORT void sub_53E2C0();
-    EXPORT void sub_53E2E0();
-
-    s32 field_0;
-    s32 field_4;
-    s32 field_8;
-    s32 field_C;
+    EXPORT char_type UpdateFloatingParticle_state_6_15_16_17_538060();
+    EXPORT char_type UpdateDirectedProjectile_state_3_12_5384C0();
+    EXPORT char_type UpdateBeamSegment_state_43_538A40();
+    EXPORT char_type UpdateObjectBeamLink_state_38_538AC0();
+    EXPORT char_type UpdateDirectedBurstSweep_state_4_539040();
+    EXPORT char_type UpdateDirectedBurst_state_13_14_36_539480();
+    EXPORT char_type UpdateCircularBurst_state_5_539890();
+    EXPORT char_type UpdateStaticAnim_state_39_53A180();
+    EXPORT char_type UpdateSkidOrScrapeSpark_state_40_41_53A280();
+    EXPORT char_type Empty_state_42_53AB70();
+    EXPORT char_type UpdateSimpleBallisticMotion_state_1_53ABA0();
+    EXPORT char_type UpdateLargeBallisticDebris_state_35_53AE60();
+    EXPORT char_type Empty_state_44_53B170();
+    EXPORT void UpdateDebrisArc_state_7_53B1A0();
+    EXPORT char_type UpdateShortAnim_state_37_53B580();
+    EXPORT char_type UpdateAttachedEmitter_state_9_10_53B670();
+    EXPORT char_type UpdateBurstAnimation_state_29_30_53B9F0();
+    EXPORT char_type UpdateCollisionBurst_state_31_34_53BAC0();
+    EXPORT s32 Service_53D260();
+    EXPORT void PoolAllocate_53E2C0();
+    EXPORT void PoolDeallocate_53E2E0();
+    
+    s32 field_0_id;
+    s32 field_4_flags;
+    s32 field_8_xpos;
+    s32 field_C_ypos;
     s32 field_10;
     s32 field_14;
     s32 field_18;
@@ -41,19 +42,18 @@ class Particle_4C
     s32 field_20;
     Ang16 field_24;
     s16 field_26;
-    s32 field_28;
-    s16 field_2C;
+    Sprite* field_28_pSprite;
+    s16 field_2C_counter;
     s16 field_2E;
-    Sprite* field_30;
+    Sprite* field_30_pNext;
     s32 field_34;
-    s32 field_38;
-    Particle_4C* mpNext;
-    s32 field_40;
-    char_type field_44;
-    char_type field_45;
-    char_type field_46;
+    s32 field_38_state;
+    Particle_4C* mpNext; // 0x3C
+    Wolfy_30* field_40_pUnknown;
+    s16 field_44;
+    u8 field_46_sub_state;
     char_type field_47;
-    char_type field_48;
+    char_type field_48_timer;
     char_type field_49;
     char_type field_4A;
     char_type field_4B;
