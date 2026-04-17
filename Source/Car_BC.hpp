ed5afee18b846ade4f0f8fea79671a3174da97f9
Jiyu <info@jiyu.dev>
2026-04-17 19:50:35 +0200

    wip miss2_0x11C::SCRCMD_THREAD_DECLARE3_504660

--- Source/Car_BC.hpp
+++ Source/Car_BC.hpp
@@ -16,6 +16,7 @@
 #include <wchar.h>
 
 struct gmp_zone_info;
+struct SCR_THREAD;
 class Camera_0xBC;
 class Object_2C;
 class Fix16_Rect;
@@ -62,7 +63,7 @@ class Car_214
 {
   public:
     EXPORT void sub_5C8680(u8 idx);
-    EXPORT char_type sub_5C86C0(s32* pType, s32* f_C, s32 f_0, Fix16 xpos, Fix16 ypos, Fix16 zpos, Fix16 a8, Fix16 a9);
+    EXPORT char_type sub_5C86C0(s32* pType, s32& f_C, SCR_THREAD* f_0, Fix16 xpos, Fix16 ypos, Fix16 zpos, Fix16 a8, Fix16 a9);
     EXPORT void sub_5C8750();
     EXPORT u16* sub_5C8780(u8 a2, Sprite* pCarSprite);
     Car_18 field_0[22];
--- Source/miss2_0x11C.cpp
+++ Source/miss2_0x11C.cpp
@@ -707,21 +707,18 @@ void miss2_0x11C::SCRCMD_THREAD_DECLARE3_504660(s32 a2)
 
     if (((SCR_POINTER*)gfrosty_pasteur_6F8060->GetBasePointer_512770(pCmd->field_10_char_idx))->field_8_char)
     {
-        Fix16 temp_x;
-        Fix16 temp_y;
         s32 type;
-        Fix16 zpos;
-        zpos.FromInt(pCmd->field_14_z);
+        Fix16 zpos(pCmd->field_14_z);
         a2 = 2;
         type = 4;
-        temp_y.FromInt(pCmd->field_13_y);
-        Fix16 ypos = ((const Fix16&)temp_y) + dword_6F75F0;
-        temp_x.FromInt(pCmd->field_12_x);
-        Fix16 xpos = ((const Fix16&)temp_x) + dword_6F75F0;
+        Fix16 temp_y(pCmd->field_13_y);
+        Fix16 ypos = temp_y + dword_6F75F0;
+        Fix16 temp_x(pCmd->field_12_x);
+        Fix16 xpos = temp_x + dword_6F75F0;
         pCmd->field_15 = gCar_214_705F20->sub_5C86C0(
             &type,
-            (s32*)&a2,
-            (s32)pCmd,
+            a2,
+            pCmd,
             xpos,
             ypos,
             zpos,
