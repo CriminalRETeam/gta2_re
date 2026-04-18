f394a061c7f2cfe7c9f36e0d00ea9c0c6f0de73f
Valps <valps.rn@gmail.com>
2026-04-18 12:19:19 -0300

    wip Sprite::IntersectsRectSAT_59FB10

--- Source/Fix16_Rect.hpp
+++ Source/Fix16_Rect.hpp
@@ -98,6 +98,26 @@ class Fix16_Rect
             false;
     }
 
+    Fix16& get_top_45ADD0()
+    {
+        return field_8_top;
+    }
+
+    Fix16& get_left_45ADB0()
+    {
+        return field_0_left;
+    }
+
+    Fix16& get_right_45ADA0()
+    {
+        return field_4_right;
+    }
+
+    Fix16& get_bottom_45ADC0()
+    {
+        return field_C_bottom;
+    }
+
     Fix16 field_0_left;
     Fix16 field_4_right;
     Fix16 field_8_top;
